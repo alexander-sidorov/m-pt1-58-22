94d5b2eeb1c3363b68b1630cbacd49122d5d8991
Alexander Sidorov <alexander@sidorov.dev>
2022-11-25 02:34:13 +0300

    CI setup

--- .github/scripts/qa.sh
+++ .github/scripts/qa.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+if [[ ! $(which poetry) ]]; then
+  dir_venv=""
+else
+  dir_venv="$(poetry env info --path)"
+fi
+
 black --check hw/
-isort --virtual-env="$(poetry env info --path)" hw/
+isort --virtual-env="${dir_venv}" hw/
 mypy
 flake8
 pytest
