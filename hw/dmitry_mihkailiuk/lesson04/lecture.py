0c5e145ca34f5fa6f05dfb0da2f84d088c2abf98
Mikhailiuk-Dmitriy <mds5@mail.ru>
2022-11-30 21:29:23 +0300

    add anatations

--- hw/dmitry_mihkailiuk/lesson04/lecture.py
+++ hw/dmitry_mihkailiuk/lesson04/lecture.py
@@ -1,11 +1,11 @@
 from decimal import Decimal
 
 
-def task_01_money(rubles, coins, amount):
-    return (rubles + (coins / 100)) * amount
+def task_01_money(rubles: int, coins: int, amount: int) -> Decimal:
+    return Decimal((rubles + (coins / 100)) * amount)
 
 
-def task_02_sign(number):
+def task_02_sign(number: int) -> int:
     try:
         Decimal(number)
         if number > 0:
@@ -18,18 +18,14 @@ def task_02_sign(number):
         return 0
 
 
-def task_03_triangle(side1, side2, side3):
+def task_03_triangle(side1: float, side2: float, side3: float) -> bool:
     if side1 + side2 > side3 and side2 + side3 > side1 and side1 + side3 > side2:
         return True
     else:
         return False
 
 
-print(task_03_triangle(3, 4, 5))
-print(task_03_triangle(1, 2, 3))
-
-
-def task_04_palindrom(string):
+def task_04_palindrom(string: str) -> bool:
     string_rep = string.lower().replace(" ", "")
     string_rev = string_rep[::-1]
 
