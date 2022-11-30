23d14a8010111f9367c228ddcc8a1f46ddc00d3c
mikitakarman <mikita.karmanaw@gmail.com>
2022-11-30 22:04:22 +0300

    v8

--- hw/mikita_karmanaw/lesson04/lecture.py
+++ hw/mikita_karmanaw/lesson04/lecture.py
@@ -1,7 +1,12 @@
 from decimal import Decimal
 
-def task_01_money(rubles, coins, amount):
-    return(Decimal((rubles + (coins / 100)) * amount))
+
+def task_01_money(rubles: int, coins: int, amount: int):
+    rb = Decimal(rubles)
+    cn = Decimal(coins)
+    am = Decimal(amount)
+    return (rb * (cn / 100)) * am
+
 
 def task_02_sign(number):
     if number == 0 or isinstance(number, complex):
@@ -10,16 +15,16 @@ def task_02_sign(number):
         sign = (1 if number > 0 else -1)
     return(sign)
 
+
 def task_03_triangle(side1, side2, side3):
     tria = [side1, side2, side3]
     for i in tria:
         if i <= 0: return(False)
     return(bool(tria.pop(tria.index(max(tria))) < (tria[0] + tria[1])))
 
+
 def task_04_palindrom(string):
     string = string.lower()
     for i in ' ,.:;!?':
         string = string.replace(i, '')
     return(bool(string == string[::-1]))
-
-print(task_02_sign(1j))
