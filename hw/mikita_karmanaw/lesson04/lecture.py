b51ac1209c96dfd2714d2d7544efac8282c0bd10
karmaxa <mikita.karmanaw@gmail.com>
2022-11-30 23:13:27 +0300

    v10

--- hw/mikita_karmanaw/lesson04/lecture.py
+++ hw/mikita_karmanaw/lesson04/lecture.py
@@ -1,14 +1,15 @@
 from decimal import Decimal
+from typing import Union
 
 
-def task_01_money(rubles: int, coins: int, amount: int) -> float:
+def task_01_money(rubles: int, coins: int, amount: int) -> Decimal:
     rb = Decimal(rubles)
     cn = Decimal(coins)
     am = Decimal(amount)
     return (rb + (cn / 100)) * am
 
 
-def task_02_sign(number) -> int:
+def task_02_sign(number: Union[int, float, complex]) -> int:
     if number == 0 or isinstance(number, complex):
         sign = 0
     else:
@@ -16,14 +17,14 @@ def task_02_sign(number) -> int:
     return sign
 
 
-def task_03_triangle(side1, side2, side3) -> bool:
+def task_03_triangle(side1: float, side2: float, side3: float) -> bool:
     tria = [side1, side2, side3]
     for i in tria:
         if i <= 0: return(False)
-    return bool(tria.pop(tria.index(max(tria))) < (tria[0] + tria[1]))
+    return tria.pop(tria.index(max(tria))) < (tria[0] + tria[1])
 
 
-def task_04_palindrom(string) -> bool:
+def task_04_palindrom(string: str) -> bool:
     string = string.lower()
     for i in ' ,.:;!?':
         string = string.replace(i, '')
