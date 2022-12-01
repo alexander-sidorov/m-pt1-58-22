6e302ce55c915995eb7473fe99bb3bd9d6400be8
Yigal Igor Maksimov <yigal.maksimov@gmail.com>
2022-12-01 16:15:32 +0300

    added test for task number 1

--- hw/igor_maksimov/lesson04/lecture_test.py
+++ hw/igor_maksimov/lesson04/lecture_test.py
@@ -1,1 +1,8 @@
+from decimal import Decimal
+from hw.igor_maksimov.lesson04.lecture import task_01_money
 
+# Test for task number 1
+
+
+def test_task_01_money() -> None:
+    assert task_01_money(11, 4, 4) == Decimal(44.16)
