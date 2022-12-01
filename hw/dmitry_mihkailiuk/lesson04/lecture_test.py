2671e3657b60b797a3e19b3d993fb41194b497ce
Mikhailiuk-Dmitriy <dmitriymikhailiuk@gmail.com>
2022-12-01 11:03:29 +0300

    fixed import, task_03_triangle, task_01_money

--- hw/dmitry_mihkailiuk/lesson04/lecture_test.py
+++ hw/dmitry_mihkailiuk/lesson04/lecture_test.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+
 from hw.dmitry_mihkailiuk.lesson04.lecture import task_01_money
 from hw.dmitry_mihkailiuk.lesson04.lecture import task_02_sign
 from hw.dmitry_mihkailiuk.lesson04.lecture import task_03_triangle
