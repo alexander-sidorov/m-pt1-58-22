09237aec91a4ff7ddfe443f1b7fbab268db3475a
AliakseiTsiukhai <leha26061992@gmal.com>
2022-12-01 12:57:43 +0300

    "еще немного правок"

--- hw/alexey_tyuhai/lesson04/lecture.py
+++ hw/alexey_tyuhai/lesson04/lecture.py
@@ -19,7 +19,9 @@ def task_02_sign(number: Any) -> int:
 
 
 def task_03_triangle(side1: int, side2: int, side3: int) -> bool:
-    if side1 + side2 > side3 and side1 + side3 > side2 and side3 + side2 > side1:
+    if side1 + side2 > side3 \
+            and side1 + side3 > side2 \
+            and side3 + side2 > side1:
         return True
     else:
         return False
