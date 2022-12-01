51527710c88d54afe9664fef72f905d6e3545352
Yigal Igor Maksimov <yigal.maksimov@gmail.com>
2022-12-01 16:06:23 +0300

    completed task number 1

--- hw/igor_maksimov/lesson04/lecture.py
+++ hw/igor_maksimov/lesson04/lecture.py
@@ -1,1 +1,8 @@
+# Task number 1
+def task_01_money(rubles: int, coins: int, amount: int) -> float:
+    float_price = float(rubles + (coins / 100))
+    print(float_price * amount)
+    return (float_price * amount)
 
+
+task_01_money(11, 4, 4)
