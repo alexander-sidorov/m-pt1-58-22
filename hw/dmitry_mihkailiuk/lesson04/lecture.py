1d5a81d849b5c8a09465bba1c30227bdd2eed986
Mikhailiuk-Dmitriy <dmitriymikhailiuk@gmail.com>
2022-12-01 18:20:38 +0300

    delite line in task_01_money, task_04_palindrom

--- hw/dmitry_mihkailiuk/lesson04/lecture.py
+++ hw/dmitry_mihkailiuk/lesson04/lecture.py
@@ -7,7 +7,6 @@ def task_01_money(rubles: int, coins: int, amount: int) -> Decimal:
     coins_dec = Decimal(coins)
     amount_dec = Decimal(amount)
     result = (rubles_dec + (coins_dec / 100)) * amount_dec
-
     return result
 
 
@@ -34,5 +33,4 @@ def task_03_triangle(side1: float, side2: float, side3: float) -> bool:
 def task_04_palindrom(string: str) -> bool:
     string_rep = string.lower().replace(" ", "")
     string_rev = string_rep[::-1]
-
     return string_rep == string_rev
