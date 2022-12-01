f7dc32bd920bf9ddd672d19110c9700d5d82c104
Yauheni_Liubimov <0.5TELOO@gmail.com>
2022-12-01 16:00:26 +0300

    fixed, added poetry

--- hw/eugene_lubimov/lesson04/lecture.py
+++ hw/eugene_lubimov/lesson04/lecture.py
@@ -18,11 +18,14 @@ def task_02_sign(number: object) -> int:
 
 def task_03_triangle(side1: int, side2: int, side3: int) -> bool:
 
-    return side1 < (side2 + side3) and side2 < (side1 + side3) and side3 < (side1 + side2)
+    return (
+        side1 < (side2 + side3)
+        and side2 < (side1 + side3)
+        and side3 < (side1 + side2)
+    )
 
 
 def task_04_palindrom(string: str) -> bool:
 
-    new_string = ''.join([i for i in string if i.isalpha()]).lower()
-    return new_string == new_string[::-1] 
-    
+    new_string = "".join([i for i in string if i.isalpha()]).lower()
+    return new_string == new_string[::-1]
