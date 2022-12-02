e761e2f15b22f590f194b4837415f7a021283f97
Yigal Igor Maksimov <yigal.maksimov@gmail.com>
2022-12-02 13:38:46 +0300

    check with poetry

--- hw/igor_maksimov/lesson04/lecture_test.py
+++ hw/igor_maksimov/lesson04/lecture_test.py
@@ -1,8 +1,7 @@
 from hw.igor_maksimov.lesson04.lecture import task_01_money
 from decimal import Decimal
 
+
 # Test for task number 1
-
-
 def test_task_01_money() -> None:
     assert task_01_money(11, 4, 4) == Decimal(44.16)
