8e123814ae4024ad90489835354b110f2e91ea6b
AliakseiTsiukhai <leha26061992@gmail.com>
2022-12-02 20:31:29 +0300

    fix

--- hw/alexey_tyuhai/lesson04/lecture_test.py
+++ hw/alexey_tyuhai/lesson04/lecture_test.py
@@ -1,27 +1,27 @@
 from decimal import Decimal
 
-from hw.alexey_tyuhai.lesson04.lecture import task_01_money as task1
-from hw.alexey_tyuhai.lesson04.lecture import task_02_sign as task2
-from hw.alexey_tyuhai.lesson04.lecture import task_03_triangle as task3
-from hw.alexey_tyuhai.lesson04.lecture import task_04_palindrom as task4
+from hw.alexey_tyuhai.lesson04.lecture import task_01_money
+from hw.alexey_tyuhai.lesson04.lecture import task_02_sign
+from hw.alexey_tyuhai.lesson04.lecture import task_03_triangle
+from hw.alexey_tyuhai.lesson04.lecture import task_04_palindrom
 
 
-def task1() -> None:
-    assert task1(1, 2, 3) == Decimal("3.06")
+def task_01_money() -> None:
+    assert task_01_money(1, 2, 3) == Decimal("3.06")
 
 
-def task2() -> None:
-    assert task2(0) == 0
-    assert task2(0.3621) == 1
-    assert task2(Decimal("2")) == 1
-    assert task2(1j) == 0
+def task_02_sign() -> None:
+    assert task_02_sign(0) == 0
+    assert task_02_sign(0.3621) == 1
+    assert task_02_sign(Decimal("2")) == 1
+    assert task_02_sign(1j) == 0
 
 
-def task3() -> None:
-    assert task3(3, 4, 5)
-    assert not task3(1, 2, 3)
+def task_03_triangle() -> None:
+    assert task_03_triangle(3, 4, 5)
+    assert not task_03_triangle(1, 2, 3)
 
 
-def task4() -> None:
-    assert task4("Аргентина манит негра")
-    assert not task4("Аргентина")
+def task_04_palindrom() -> None:
+    assert task_04_palindrom("Аргентина манит негра")
+    assert not task_04_palindrom("Аргентина")
