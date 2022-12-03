2383c2d5fb3b6a0eced4832344fd383bfd17ce58
Sergienko95 <jana_monkey@mail.ru>
2022-12-03 18:00:00 +0300

    hw sergienko

---
+++ hw/jana_sergienko/lesson04/lecture.py
@@ -0,0 +1,43 @@
+from decimal import Decimal
+
+def task_01_money(rubles: int, coins: int, amount: int) -> Decimal:
+
+    rubles = Decimal(rubles)
+    coins = Decimal(coins)
+    amount = Decimal(amount)
+
+    return (rubles + (coins/100)) * amount
+
+def task_02_sign(number) -> int:
+
+    if type(number) == complex:
+        return 0
+    else:
+        if number > 0:
+            return 1
+        elif number < 0:
+            return -1
+        else:
+            return 0
+
+def task_03_triangle(side1: float, side2: float, side3: float) -> bool:
+
+    if side1 > 0 and side2 > 0 and side3 > 0:
+        if (side1 + side2) > side3 and (side2 + side3) > side1 and (side1 + side3) > side2:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def task_04_palindrom(string: str) -> bool:
+
+    string = string.replace(" ", "").lower()
+    str_ = string[::-1]
+    return string == str_
+
+
+
+
+
+
---
+++ hw/jana_sergienko/lesson04/lecture_test.py
@@ -0,0 +1,19 @@
+from decimal import Decimal
+from lecture import task_01_money
+from lecture import task_02_sign
+from lecture import task_03_triangle
+from lecture import task_04_palindrom
+
+assert task_01_money(1, 2, 3) == Decimal("3.06")
+
+assert task_02_sign(0) == 0
+assert task_02_sign(0.3621) == 1
+assert task_02_sign(-123) == -1
+assert task_02_sign(Decimal("2")) == 1
+assert task_02_sign(1j) == 0
+
+assert task_03_triangle(3, 4, 5)
+assert not task_03_triangle(1, 2, 3)
+
+assert task_04_palindrom("Аргентина манит негра")
+assert not task_04_palindrom("Аргентина")
