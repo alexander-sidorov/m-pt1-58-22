80f359df7cbeb0c56410d99dcebf4bc26ab227de
Sergienko95 <jana_monkey@mail.ru>
2022-12-04 11:54:34 +0300

    changes

--- hw/jana_sergienko/lesson04/lecture.py
+++ hw/jana_sergienko/lesson04/lecture.py
@@ -6,7 +6,7 @@ def task_01_money(rubles: int, coins: int, amount: int) -> Decimal:
     rub = Decimal(rubles)
     coin = Decimal(coins)
     amt = Decimal(amount)
-    return (rub + (coin/100)) * amt
+    return (rub + (coin / 100)) * amt
 
 
 def task_02_sign(number: Any) -> int:
@@ -22,13 +22,13 @@ def task_02_sign(number: Any) -> int:
 
 
 def task_03_triangle(side1: float, side2: float, side3: float) -> bool:
-    if side1 > 0 and side2 > 0 and side3 > 0:
-        if (side1 + side2) > side3 and (side2 + side3) > side1 and (side1 + side3) > side2:
-            return True
-        else:
-            return False
-    else:
-        return False
+        tringle = [side1, side2, side3]
+        for i in tringle:
+            if i <= 0:
+                return False
+        return (tringle[0] + tringle[1] > tringle[2]
+                and tringle[1] + tringle[2] > tringle[0]
+                and tringle[0] + tringle[2] > tringle[1])
 
 
 def task_04_palindrom(string: str) -> bool:
