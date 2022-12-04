ea870e8a46ef7d482cbbb03654cab18039ec3716
Vladislav Yurenya <228322fsa@mail.ru>
2022-12-04 11:42:41 +0300

    replace name

--- hw/vladislav_yurenya/lesson_04/lecture_test.py
+++ hw/vladislav_yurenya/lesson_04/lecture_test.py
@@ -1,9 +1,9 @@
 from decimal import Decimal
 
-from hw.vadim_zharski.lesson_04.lecture import task_01_money
-from hw.vadim_zharski.lesson_04.lecture import task_02_sign
-from hw.vadim_zharski.lesson_04.lecture import task_03_triangle
-from hw.vadim_zharski.lesson_04.lecture import task_04_palindrom
+from hw.vladislav_yurenya.lesson_04.lecture import task_01_money
+from hw.vladislav_yurenya.lesson_04.lecture import task_02_sign
+from hw.vladislav_yurenya.lesson_04.lecture import task_03_triangle
+from hw.vladislav_yurenya.lesson_04.lecture import task_04_palindrom
 
 
 def test_task_01_money() -> None:
