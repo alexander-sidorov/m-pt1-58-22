08fbbaaa91bb426b9e4e982991e71809c7fdac9e
Yigal Igor Maksimov <yigal.maksimov@gmail.com>
2022-12-05 15:14:59 +0300

    finish lesson04 tasks

--- hw/igor_maksimov/lesson04/lecture.py
+++ hw/igor_maksimov/lesson04/lecture.py
@@ -1,8 +1,46 @@
+from decimal import Decimal
+from typing import Any
+
+
 # Task number 1
-def task_01_money(rubles: int, coins: int, amount: int) -> float:
-    float_price = float(rubles + (coins / 100))
-    print(float_price * amount)
-    return (float_price * amount)
 
 
-task_01_money(11, 4, 4)
+def task_01_money(rubles: int, coins: int, amount: int) -> Decimal:
+    rubles = Decimal(rubles)
+    coins = Decimal(coins)
+    amount = Decimal(amount)
+    total = (rubles + (coins / 100)) * amount
+    return total
+
+
+# Task number 2
+
+
+def task_02_sign(number: Any) -> int:
+    if number > 0:
+        return +1
+    elif number < 0:
+        return -1
+    else:
+        return 0
+
+
+# Task number 3
+
+def task_03_triangle(side1: float, side2: float, side3: float) -> bool:
+    result = (side1 + side2 > side3 and side1 + side3 > side2 and side2 + side3 > side1)
+    if result:
+        print('Its triangle!!')
+    else:
+        print("Its not triangle")
+    return result
+
+
+# Task number 4
+
+
+def task_04_palindrom(string: str) -> bool:
+    string = string.lower()
+    string = string.replace(" ", "")
+    result = string[::-1] == string
+    return result
--- hw/igor_maksimov/lesson04/lecture_test.py
+++ hw/igor_maksimov/lesson04/lecture_test.py
@@ -1,7 +1,30 @@
 from hw.igor_maksimov.lesson04.lecture import task_01_money
+from hw.igor_maksimov.lesson04.lecture import task_02_sign
+from hw.igor_maksimov.lesson04.lecture import task_03_triangle
+from hw.igor_maksimov.lesson04.lecture import task_04_palindrom
+
+
 from decimal import Decimal
 
 
 # Test for task number 1
 def test_task_01_money() -> None:
-    assert task_01_money(11, 4, 4) == Decimal(44.16)
+    assert task_01_money(1, 2, 3) == Decimal("3.06")
+
+
+def test_task_02_sign() -> None:
+    assert task_02_sign(0) == 0
+    assert task_02_sign(0.3621) == 1
+    assert task_02_sign(-123) == -1
+    assert task_02_sign(Decimal("2")) == 1
+    assert task_02_sign(1j) == 0
+
+
+def test_task_03_triangle() -> None:
+    assert task_03_triangle(3, 4, 5)
+    assert not task_03_triangle(1, 2, 3)
+
+
+def test_task_04_palindrom() -> None:
+    assert task_04_palindrom("Аргентина манит негра")
+    assert not task_04_palindrom("Аргентина")
