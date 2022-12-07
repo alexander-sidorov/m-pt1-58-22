156ed95cc4721c8d025b9f9369c440d6952476b5
Yauheni_Liubimov <0.5TELOO@gmail.com>
2022-12-07 16:26:57 +0300

    fixed

--- hw/eugene_lubimov/lesson06/tasks.py
+++ hw/eugene_lubimov/lesson06/tasks.py
@@ -1,3 +1,6 @@
+from typing import Any
+from typing import Union
+
 from hw.alexander_sidorov.helpers import CITIES
 
 CITIES = {
@@ -32,24 +35,26 @@ CITIES = {
 }
 
 
-def task_01_boundary(sequence: object) -> tuple:
+def task_01_boundary(sequence: Union[str, list, tuple]) -> tuple:
 
     return sequence[0], sequence[-1]
 
 
-def task_02_expand(sequence: object) -> object:
+def task_02_expand(sequence: Any) -> Any:
 
     return sequence[1:] * sequence[0]
 
 
-def task_03_hdist(seq1: object, seq2: object) -> int:
+def task_03_hdist(
+    seq1: Union[str, list, tuple], seq2: Union[str, list, tuple]
+) -> int:
 
     return sum(True for i in zip(seq1, seq2) if i[0] != i[1]) + abs(
         len(seq1) - len(seq2)
     )
 
 
-def get_distance(point1: tuple, point2: tuple) -> float:
+def get_distance(point1: tuple, point2: tuple) -> Any:
 
     d_x, d_y = (point1[0] - point2[0]) * 111, (point1[1] - point2[1]) * 65
     return (d_x**2 + d_y**2) ** 0.5
@@ -60,9 +65,9 @@ def task_04_cities(city: str) -> dict:
     return {k: get_distance(CITIES[city], v) for k, v in CITIES.items()}
 
 
-def task_05_route(route: object) -> float:
+def task_05_route(route: Union[list, tuple]) -> float:
 
-    route_len, route_city = 0, [city for city in route if city in CITIES]
+    route_len, route_city = 0.0, [city for city in route if city in CITIES]
     for i in range(len(route_city) - 1):
         route_len += get_distance(
             CITIES[route_city[i]], CITIES[route_city[i + 1]]
