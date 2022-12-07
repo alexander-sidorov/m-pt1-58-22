a3239ce1c573825cf5d139d4080ded021ef9fa92
Yauheni_Liubimov <0.5TELOO@gmail.com>
2022-12-07 22:45:04 +0300

    fixed

--- hw/eugene_lubimov/lesson06/tasks.py
+++ hw/eugene_lubimov/lesson06/tasks.py
@@ -1,53 +1,20 @@
+from typing import Sequence
 from typing import Any
-from typing import Union
 
 from hw.alexander_sidorov.helpers import CITIES
 
-CITIES = {
-    "Барановичи": (53.132227, 26.017363),
-    "Берёза": (52.534754, 24.984273),
-    "Бобруйск": (53.145646, 29.225297),
-    "Борисов": (54.224141, 28.511507),
-    "Браслав": (55.639499, 27.031569),
-    "Брест": (52.093721, 23.684757),
-    "Витебск": (55.184204, 30.202767),
-    "Волковыск": (53.162982, 24.463780),
-    "Глубокое": (55.138798, 27.685619),
-    "Гомель": (52.424280, 31.014063),
-    "Гродно": (53.677764, 23.829300),
-    "Житковичи": (52.217048, 27.854883),
-    "Жлобин": (52.892235, 30.037624),
-    "Жодино": (54.094370, 28.321572),
-    "Кобрин": (52.208612, 24.354257),
-    "Лепель": (54.886647, 28.693817),
-    "Лида": (53.891662, 25.302200),
-    "Минск": (53.902735, 27.555696),
-    "Мозырь": (52.049078, 29.267184),
-    "Молодечно": (54.307345, 26.838960),
-    "Орша": (54.510823, 30.429367),
-    "Пинск": (52.111481, 26.102166),
-    "Полоцк": (55.485590, 28.767977),
-    "Поставы": (55.113615, 26.839303),
-    "Речица": (52.371565, 30.386450),
-    "Светлогорск": (52.633044, 29.748194),
-    "Слуцк": (53.027615, 27.552063),
-    "Солигорск": (52.792866, 27.543479),
-}
 
-
-def task_01_boundary(sequence: Union[str, list, tuple]) -> tuple:
+def task_01_boundary(sequence: Sequence) -> tuple:
 
     return sequence[0], sequence[-1]
 
 
-def task_02_expand(sequence: Any) -> Any:
+def task_02_expand(sequence: Sequence) -> Any:
 
     return sequence[1:] * sequence[0]
 
 
-def task_03_hdist(
-    seq1: Union[str, list, tuple], seq2: Union[str, list, tuple]
-) -> int:
+def task_03_hdist(seq1: Sequence, seq2: Sequence) -> int:
 
     return sum(True for i in zip(seq1, seq2) if i[0] != i[1]) + abs(
         len(seq1) - len(seq2)
@@ -65,7 +32,7 @@ def task_04_cities(city: str) -> dict:
     return {k: get_distance(CITIES[city], v) for k, v in CITIES.items()}
 
 
-def task_05_route(route: Union[list, tuple]) -> float:
+def task_05_route(route: list | tuple) -> float:
 
     route_len, route_city = 0.0, [city for city in route if city in CITIES]
     for i in range(len(route_city) - 1):
