8d73ee856ad5d82739bb2ae0a16906003e376635
Mikhailiuk-Dmitriy <dmitriymikhailiuk@gmail.com>
2022-12-07 08:42:28 +0300

    added methods test_tuple and test_list

--- hw/dmitry_mihkailiuk/lesson05/stdtypes_test.py
+++ hw/dmitry_mihkailiuk/lesson05/stdtypes_test.py
@@ -118,3 +118,113 @@ def test_str() -> None:
     assert "abc".upper() == "ABC"
 
     assert "abc".zfill(5) == "00abc"
+
+
+def test_tuple() -> None:
+    # __add__
+    assert ("a",) + ("b",) == ("a", "b")
+
+    # __contains__
+    assert "a" in ("a", "b", "c")
+
+    # __getitem__
+    assert ("a", "b", "c")[1] == "b"
+
+    # __hash__
+    assert isinstance(("a", "b", "c"), Hashable)
+
+    # __len__
+    assert len(("a", "b", "c")) == 3
+
+    # __mul__
+    assert ("a", "b", "c") * 2 == ("a", "b", "c", "a", "b", "c")
+
+    assert ("a",) < ("b",)
+
+    assert ("a", "b", "c", "d", "e", "b").count("b") == 2
+
+    assert ("a", "b", "c", "d", "e", "b").index("b") == 1
+
+
+def test_list() -> None:
+    # __add__
+    assert ["a"] + ["b"] == ["a", "b"]
+
+    # __contains__
+    assert "a" in ["a", "b", "c"]
+
+    # __delitem__
+    list1 = ["a", "b", "c"]
+    del list1[1]
+    assert list1 == ["a", "c"]
+
+    # __getitem__
+    assert ["a", "b", "c"][2] == "c"
+
+    # __hash__
+    assert not isinstance(["a", "b", "c"], Hashable)
+
+    # __iadd__
+    list1 = ["a"]
+    list1 += ["b"]
+    assert list1 == ["a", "b"]
+
+    # __imul__
+    list1 = ["a"]
+    list1 *= 2
+    assert list1 == ["a", "a"]
+
+    # __len__
+    assert len(["a", "b", "c"]) == 3
+
+    # __mul__
+    assert ["a", "b"] * 2 == ["a", "b", "a", "b"]
+
+    # __setitem__
+    list1 = ["a", "b"]
+    list1[1] = "c"
+    assert list1 == ["a", "c"]
+
+    assert ["a"] < ["a", "b"]
+
+    list1 = ["a"]
+    list1.append("b")
+    assert list1 == ["a", "b"]
+
+    list1 = ["a", "b"]
+    list1.clear()
+    assert list1 == []
+
+    list1 = ["a"]
+    list2 = list1.copy()
+    assert list2 == ["a"]
+
+    assert ["a", "c", "b", "c"].count("c") == 2
+
+    list1 = ["a", "b"]
+    list1.extend(["c", "d"])
+    assert list1 == ["a", "b", "c", "d"]
+
+    assert ["a", "b", "c"].index("b") == 1
+
+    list1 = ["a", "c"]
+    list1.insert(1, "b")
+    assert list1 == ["a", "b", "c"]
+
+    list1 = ["a", "b", "c"]
+    list1.pop(1)
+    assert list1 == ["a", "c"]
+
+    list1 = ["a", "b", "c"]
+    list1.remove("a")
+    assert list1 == ["b", "c"]
+
+    list1 = ["a", "b", "c"]
+    list1.reverse()
+    assert list1 == ["c", "b", "a"]
+
+    list1 = ["c", "b", "a", "a", "b", "c"]
+    list1.sort()
+    assert list1 == ['a', 'a', 'b', 'b', 'c', 'c']
+
+
