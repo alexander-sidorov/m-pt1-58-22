57ef386cb97fab7af49240bd79100bd398ce0726
Yauheni_Liubimov <0.5TELOO@gmail.com>
2022-12-07 14:33:53 +0300

    fixed

--- hw/eugene_lubimov/lesson06/tasks.py
+++ hw/eugene_lubimov/lesson06/tasks.py
@@ -1,5 +1,4 @@
-#from hw.alexander_sidorov.helpers import CITIES
-
+from hw.alexander_sidorov.helpers import CITIES
 
 CITIES = {
     "Барановичи": (53.132227, 26.017363),
@@ -30,33 +29,42 @@ CITIES = {
     "Светлогорск": (52.633044, 29.748194),
     "Слуцк": (53.027615, 27.552063),
     "Солигорск": (52.792866, 27.543479),
-}      
+}
+
 
 def task_01_boundary(sequence: object) -> tuple:
 
     return sequence[0], sequence[-1]
 
+
 def task_02_expand(sequence: object) -> object:
 
     return sequence[1:] * sequence[0]
 
+
 def task_03_hdist(seq1: object, seq2: object) -> int:
 
-    #return sum(map(lambda x: x[0] != x[1], zip(seq1, seq2))) + abs(len(seq1) - len(seq2))
-    return sum(True for i in zip(seq1, seq2) if i[0] != i[1]) + abs(len(seq1) - len(seq2))
+    return sum(True for i in zip(seq1, seq2) if i[0] != i[1]) + abs(
+        len(seq1) - len(seq2)
+    )
+
 
 def get_distance(point1: tuple, point2: tuple) -> float:
 
     d_x, d_y = (point1[0] - point2[0]) * 111, (point1[1] - point2[1]) * 65
-    return (d_x ** 2 + d_y ** 2) ** 0.5
+    return (d_x**2 + d_y**2) ** 0.5
+
 
 def task_04_cities(city: str) -> dict:
 
-    return {k: get_distance(CITIES[city], v) for k, v in CITIES.items()}  
+    return {k: get_distance(CITIES[city], v) for k, v in CITIES.items()}
+
 
 def task_05_route(route: object) -> float:
 
     route_len, route_city = 0, [city for city in route if city in CITIES]
     for i in range(len(route_city) - 1):
-        route_len += get_distance(CITIES[route_city[i]], CITIES[route_city[i + 1]])
-    return route_len      
+        route_len += get_distance(
+            CITIES[route_city[i]], CITIES[route_city[i + 1]]
+        )
+    return route_len
