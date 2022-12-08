0d0f219c4b1692750c6c71512096e8e94b2ed291
Eugene Ladyko <angeby13@gmail.com>
2022-12-09 01:46:36 +0300

    1 and 2 task

--- hw/eugene_ladyko/lesson04/lecture.py
+++ hw/eugene_ladyko/lesson04/lecture.py
@@ -1,1 +1,18 @@
-Режим вывода команд на экран (ECHO) включен.
+from decimal import Decimal
+from typing import Any
+
+
+def task_01_money(rubles: int, coins: int, amount: int) -> Decimal:
+    ru = Decimal(rubles)
+    co = Decimal(coins)
+    am = Decimal(amount)
+    return (ru + co / 100) * am
+
+
+def task_02_sing(numb: Any) -> int:
+    if type(numb) == complex or numb == 0:
+        return 0
+    elif numb > 0:
+        return 1
+    elif numb < 0:
+        return -1
