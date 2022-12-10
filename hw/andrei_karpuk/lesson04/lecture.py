2a66195502e6fc9be8c2c52ad900161f1287fc03
Andrei Karpuk <andreigalardo@gmail.com>
2022-12-10 20:54:46 +0300

    переделанная ДЗ лессон 04 и тесты в ней

--- hw/andrei_karpuk/lesson04/lecture.py
+++ hw/andrei_karpuk/lesson04/lecture.py
@@ -2,29 +2,21 @@ from decimal import Decimal
 
 
 def task_01_money(rubles: int, coin: int, amount: int) -> Decimal:
+    return Decimal(rubles + coin / 100) * amount
 
-    return (rubles + coin/100) * amount
 
-
-def task_02_sign(number: int | float | complex) -> int:
-
-    return +1 if task_02_sign(number) > 0 else(-1 if task_02_sign(number) < 0 else 0)
+def task_02_sign(number: int | float | complex | Decimal) -> int:
+    return 0 if isinstance(number, complex) or number == 0 else (+1 if number > 0 else -1)
 
 
 def task_03_triangle(side1: float, side2: float, side3: float) -> bool:
     s1 = side1
     s2 = side2
     s3 = side3
-    if s1 + s2 > s3 and s2 + s3 > s1 and s3 + s1 > s2:
-        return True
-    else:
-        False
+    return bool(s1 + s2 > s3 and s2 + s3 > s1 and s3 + s1 > s2)
 
 
 def task_04_palindrom(string: str) -> bool:
     string = string.lower()
     string = string.replace(" ", "")
-    if string == string[::-1]:
-        return True
-    else:
-        return False
+    return bool(string == string[::-1])
--- hw/andrei_karpuk/lesson04/lecture_test.py
+++ hw/andrei_karpuk/lesson04/lecture_test.py
@@ -1,27 +1,24 @@
 from decimal import Decimal
-
-from lecture import task_01_money
-
+from hw.andrei_karpuk.lesson04.lecture import task_01_money
 from lecture import task_02_sign
-
 from lecture import task_03_triangle
-
 from lecture import task_04_palindrom
 
+def test_01_money () -> None:
 
 assert task_01_money(1, 2, 3) == Decimal("3.06")
 
-
+def test_02_sign() -> None:
 assert task_02_sign(0) == 0
 assert task_02_sign(0.3621) == 1
 assert task_02_sign(-123) == -1
 assert task_02_sign(Decimal("2")) == 1
 assert task_02_sign(1j) == 0
 
-
+def test_03_triangle() -> None:
 assert task_03_triangle(3, 4, 5)
 assert not task_03_triangle(1, 2, 3)
 
-
+def test_04_palindrom() ->None:
 assert task_04_palindrom("Аргентина манит негра")
 assert not task_04_palindrom("Аргентина")
