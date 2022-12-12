0fdfb7010343faffd28c76ef989b421eebba00b9
Vladislav Yurenya <228322fsa@mail.ru>
2022-12-12 10:25:21 +0300

    fixed

--- hw/vladislav_yurenya/lesson06/tasks.py
+++ hw/vladislav_yurenya/lesson06/tasks.py
@@ -12,20 +12,9 @@ def task_02_expand(sequence: Sequence) -> Any:
     return sequence[1:] * sequence[0]
 
 
-def f(seq1: list, seq2: list) -> list:
-    if len(seq1) < len(seq2):
-        seq1 += " " * (len(seq2) - len(seq1))
-        return seq1
-    else:
-        seq2 += " " * (len(seq1) - len(seq2))
-        return seq2
-
-
 def task_03_hdist(seq1: Sequence, seq2: Sequence) -> int:
     total = 0
-    seq1 = list(seq1)
-    seq2 = list(seq2)
-    f(seq1, seq2)
+    total += abs(len(seq1) - len(seq2))
     for zip_seq in zip(seq1, seq2):
         if zip_seq[0] != zip_seq[1]:
             total += 1
@@ -35,8 +24,8 @@ def task_03_hdist(seq1: Sequence, seq2: Sequence) -> int:
 def task_04_cities(city: str) -> dict:
     total = {}
     for key in CITIES.keys():
-        one = list(CITIES[city])
-        two = list(CITIES[key])
+        one = CITIES[city]
+        two = CITIES[key]
         x, y = (one[0] - two[0]) * 111, (one[1] - two[1]) * 65
         distance = int((x**2 + y**2) ** 0.5)
         total.update({key: distance})
@@ -45,10 +34,14 @@ def task_04_cities(city: str) -> dict:
 
 def task_05_route(route: Sequence) -> int:
     total = 0
-    for first, second in zip(route, route[1:]):
-        one = list(CITIES[first])
-        two = list(CITIES[second])
-        x, y = (one[0] - two[0]) * 111, (one[1] - two[1]) * 65
-        distance = int((x**2 + y**2) ** 0.5)
+    val = ()
+    for i in route:
+        val += CITIES[i]
+    first = val[0::2]
+    sec = val[1::2]
+    for ind in range(len(first) - 1):
+        lon = (first[ind] - first[ind + 1]) * 111
+        lat = (sec[ind] - sec[ind + 1]) * 65
+        distance = int((lon**2 + lat**2) ** 0.5)
         total += distance
     return total
