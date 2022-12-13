f04af2fb2af8bc20104f7808c664e2b6e341a06e
Sergienko95 <jana_monkey@mail.ru>
2022-12-13 16:23:56 +0300

    'split'

--- hw/jana_sergienko/lesson05/stdtypes_test.py
+++ hw/jana_sergienko/lesson05/stdtypes_test.py
@@ -39,7 +39,7 @@ def test_str() -> None:
     assert "janna".rpartition("n") == ("jan", "n", "a")
     assert "jan".rsplit("a") == ["j", "n"]
     assert "jana.!".rstrip(".!") == "jana"
-    assert "jana".split(" ") == ["jana"]
+    assert '["jana"]'.split(" ") == ["jana"]
     assert "jana\njana\njana".splitlines() == ["jana", "jana", "jana"]
     assert "jana".startswith("j")
     assert "    jana    ".strip() == "jana"
