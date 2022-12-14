e6f9074f58bc6cd8a90380a1192ad4195e34165f
karmaxa <mikita.karmanaw@gmail.com>
2022-12-14 16:13:04 +0300

    lesson09ver4

--- hw/mikita_karmanaw/lesson09/tasks_test.py
+++ hw/mikita_karmanaw/lesson09/tasks_test.py
@@ -96,7 +96,7 @@ def test_cache() -> None:
     assert not func_total_cache
     cached_function(5)
     assert "cached_function&argument=&(5,)" in func_total_cache
-    assert func_total_cache["cached_function&argument=&(5,)"] is list
+    assert isinstance(func_total_cache["cached_function&argument=&(5,)"], list)
     assert func_total_cache["cached_function&argument=&(5,)"][0] == 6
     assert func_total_cache["cached_function&argument=&(5,)"][1] > 1
     assert func_total_cache["cached_function&argument=&(5,)"][2] == 1
