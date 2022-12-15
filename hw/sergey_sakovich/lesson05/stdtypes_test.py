9f249b2f3231e98713c3751b4dc8e892ef5a9f93
SergeiSakovich <119007148+SergeiSakovich@users.noreply.github.com>
2022-12-15 19:46:42 +0300

    five_7

--- hw/sergey_sakovich/lesson05/stdtypes_test.py
+++ hw/sergey_sakovich/lesson05/stdtypes_test.py
@@ -219,6 +219,6 @@ def test_stdtypes() -> None:
     assert not isinstance(dict_13, Hashable)
     dict_13.clear()
     assert not dict_13
-    dict_13 = {1: 'python', 2: 'c++'}
-    dict_13 |= {3: 'c'}
-    assert dict_13 == {1: 'python', 2: 'c++', 3: 'c'}
+    dict_13 = {13: '13', 19: '19'}
+    dict_13 |= {7: '7'}
+    assert dict_13 == {13: '13', 19: '19', 7: '7'}
