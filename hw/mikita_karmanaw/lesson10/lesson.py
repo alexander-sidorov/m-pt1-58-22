0511d67175b7f447806977b9176d9ef34d9575ba
mikitakarman <mikita.karmanaw@gmail.com>
2022-12-16 20:12:32 +0300

    task-1-ver-2

--- hw/mikita_karmanaw/lesson10/lesson.py
+++ hw/mikita_karmanaw/lesson10/lesson.py
@@ -1,17 +1,17 @@
 class User:
     class_name = 'User'
 
-    def __init__(self, nm):
+    def __init__(self, nm) -> None:
         User.name = nm
 
     @classmethod
-    def get_user_name(cls):
+    def get_user_name(cls) -> str:
         return User.name
 
     @staticmethod
-    def get_class_name():
+    def get_class_name() -> str:
         return User.class_name
 
     @staticmethod
-    def get_hello_world():
+    def get_hello_world() -> str:
         return "hello world"
--- hw/mikita_karmanaw/lesson10/lesson_test.py
+++ hw/mikita_karmanaw/lesson10/lesson_test.py
@@ -6,3 +6,7 @@ def test_01() -> None:
     assert alice.get_user_name() == "Alice"
     assert alice.get_class_name() == "User"
     assert alice.get_hello_world() == "hello world"
+    bob = User('Bob')
+    assert bob.get_user_name() == "Bob"
+    assert bob.get_class_name() == "User"
+    assert bob.get_hello_world() == "hello world"
