a2f2bea75138947b4f1cc4adb6da5c64d65b726d
eugene_lubimov <you@example.com>
2022-12-16 21:12:35 +0300

    Zadanie 2

--- hw/eugene_lubimov/lesson10/lesson.py
+++ hw/eugene_lubimov/lesson10/lesson.py
@@ -1,13 +1,27 @@
 class User:
     def __init__(self, name: str) -> None:
-        self.name = name
+        self._name = name
 
+    @property
     def get_name(self) -> str:
-        return self.name
+        return self._name
 
+  
     @classmethod
     def get_class_name(cls) -> str:
         return cls.__name__
 
     def get_hello_world(self) -> str:
         return "hello world"
+
+
+class Counter:
+    def __init__(self, start: int, end: int) -> None:
+        self.start = start
+        self.end = end
+        self.num = start - 1
+
+    def next(self) -> int | None:  # noqa: A003
+        if self.num < self.end:
+            self.num += 1
+            return self.num
--- hw/eugene_lubimov/lesson10/lesson_test.py
+++ hw/eugene_lubimov/lesson10/lesson_test.py
@@ -2,8 +2,22 @@ import hw.eugene_lubimov.lesson10.lesson as les
 
 
 def test_user() -> None:
-    person = les.User("Bob")
+    hw_text = "hello world"
+    person1 = les.User("Bob")
 
-    assert person.get_name() == "Bob"
-    assert person.get_class_name() == "User"
-    assert person.get_hello_world() == "hello world"
+    assert person1.get_name == "Bob"
+    assert person1.get_class_name() == les.User.__name__
+    assert person1.get_hello_world() == hw_text
+   
+    person2 = les.User("Vasya")
+
+    assert person2.get_name == "Vasya"
+    assert person2.get_class_name() == les.User.__name__
+    assert person2.get_hello_world() == hw_text
+
+
+def test_counter() -> None:
+    coun = les.Counter(10, 20)
+
+    assert coun.next() == 10
+    assert coun.next() == 11
