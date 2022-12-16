b3eb9e1581eb33025a2d4985512e080cebd8d710
Yigal Maksimov <yiga.maksimov@gmail.com>
2022-12-16 21:50:13 +0300

    zadanie 2

--- hw/igor_maksimov/lesson10/lesson10.py
+++ hw/igor_maksimov/lesson10/lesson10.py
@@ -9,3 +9,18 @@ class User:
     
     def get_hello_world(self):
         return "hello world"
+
+class Counter:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.current = self.start
+  
+    def next(self)-> int:
+        if self.current > self.end:
+            return self.end
+        result, self.current = self.current, self.current + 1
+        return result
+
+
+
--- hw/igor_maksimov/lesson10/lesson10_test.py
+++ hw/igor_maksimov/lesson10/lesson10_test.py
@@ -1,4 +1,5 @@
 from hw.igor_maksimov.lesson10.lesson10 import User
+from hw.igor_maksimov.lesson10.lesson10 import Counter
 
 def test_01():
     igor = User("Igor")
@@ -10,3 +11,13 @@ def test_01():
     assert vasia.get_class_name() == User.__name__
     assert vasia.get_hello_world() == 'hello world'
 
+def test_02():
+    r1 = 1
+    ctr1 = Counter(0, r1)
+
+    r2 = 2
+    ctr2 = Counter(0, r2)
+
+    assert [ctr1.next() for _ in range(r1 + 1)] == [0, 1]
+    assert [ctr2.next() for _ in range(r2 + 1)] == [0, 1, 2]
+    assert [ctr2.next() for _ in range(r2 + 1)] == [2, 2, 2]
