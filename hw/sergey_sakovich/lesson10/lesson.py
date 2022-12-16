2af7f6a32ab6b862f03710627455f5c2c01907fe
SergeiSakovich <stem692@gmail.com>
2022-12-16 20:32:53 +0300

    lesson

---
+++ hw/sergey_sakovich/lesson10/lesson.py
@@ -0,0 +1,12 @@
+class User:
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_class_name(self) -> str:
+        return User.__name__
+
+    def get_hello_world(self) -> str:
+        return 'hello world'
