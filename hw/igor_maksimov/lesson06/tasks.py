3a85f999672ae52d6499fc1056bd6395c7504278
Yigal Igor Maksimov <yigal.maksimov@gmail.com>
2022-12-16 12:01:43 +0300

    xxx

--- hw/igor_maksimov/lesson06/tasks.py
+++ hw/igor_maksimov/lesson06/tasks.py
@@ -1,5 +1,7 @@
-from typing import Sequence
 from typing import Any
+from math import sqrt
+from typing import Sequence
+
 from hw.alexander_sidorov.helpers import CITIES
 
 
@@ -21,8 +23,18 @@ def task_03_hdist(seq1: Sequence, seq2: Sequence) -> int:
         return y
 
 
-def task_04_cities(city: str) -> float:
-    print(CITIES)
+def dist(city1: tuple, city2: tuple) -> float:
+    total_att = city1[0] - city2[0] * 111
+    total_lng = city1[1] - city2[1] * 65
+    return sqrt((total_att**2) + (total_lng**2))
 
 
-task_04_cities()
+def task_04_cities(city: str) -> dict:
+    dist_dict = {}
+    for town in CITIES:
+        dist = dist(CITIES[city], CITIES[town])
+        dist_dict[town] = dist
+    return dist_dict
+
+
+print(task_04_cities("Минск"))
