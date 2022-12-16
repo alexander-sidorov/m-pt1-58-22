20238cd61d670057f9bf41dc8425b6e4b5b354f6
Mikhailiuk-Dmitriy <mds5@mail.ru>
2022-12-16 20:00:55 +0300

    task 1

---
+++ hw/dmitry_mihkailiuk/lesson10/lesson.py
@@ -0,0 +1,13 @@
+class User:
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+    def get_class_name(self):
+        return User.__name__
+
+    def get_hello_world(self):
+        return "hello world"
