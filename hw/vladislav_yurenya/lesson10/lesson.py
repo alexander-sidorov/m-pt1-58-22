ee939952093b1c43a41eff4476242f7fe187dd27
Vladislav Yurenya <228322fsa@mail.ru>
2022-12-16 21:57:51 +0300

    finish

--- hw/vladislav_yurenya/lesson10/lesson.py
+++ hw/vladislav_yurenya/lesson10/lesson.py
@@ -10,3 +10,19 @@ class User:
 
     def get_hello_word(self) -> str:
         return "hello word"
+
+
+class Counter:
+    def __init__(self, start: int, stop: int):
+        self.start = start
+        self.stop = stop
+        self.resault = self.start
+
+    def next(self) -> int:  # noqa: A003
+        if self.resault > self.stop:
+            return self.stop
+        result, self.resault = self.resault, self.resault + 1
+        return result
+
+
+c = Counter(10, 20)
--- hw/vladislav_yurenya/lesson10/lesson_test.py
+++ hw/vladislav_yurenya/lesson10/lesson_test.py
@@ -1,3 +1,4 @@
+from hw.vladislav_yurenya.lesson10.lesson import Counter
 from hw.vladislav_yurenya.lesson10.lesson import User
 
 
@@ -11,3 +12,13 @@ def test_01() -> None:
     assert vasya.get_name() == "Вася"
     assert vasya.get_class_name() == User.__name__
     assert vasya.get_hello_word() == txt
+
+
+def test_02() -> None:
+    r1 = 1
+    ctr1 = Counter(0, r1)
+    r2 = 2
+    ctr2 = Counter(0, r2)
+    assert [ctr1.next() for _ in range(r1 + 1)] == [0, 1]
+    assert [ctr2.next() for _ in range(r2 + 1)] == [0, 1, 2]
+    assert [ctr2.next() for _ in range(r2 + 1)] == [2, 2, 2]
