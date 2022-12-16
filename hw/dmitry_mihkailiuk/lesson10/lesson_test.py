e93facb0f900853069852d0313c5a831c3dac2f4
Mikhailiuk-Dmitriy <mds5@mail.ru>
2022-12-16 20:36:09 +0300

    task 1-5

--- hw/dmitry_mihkailiuk/lesson10/lesson_test.py
+++ hw/dmitry_mihkailiuk/lesson10/lesson_test.py
@@ -4,5 +4,5 @@ from hw.dmitry_mihkailiuk.lesson10.lesson import User
 def test_01() -> None:
     dim = User("Dim")
     assert dim.get_name() == "Dim"
-    assert dim.gget_class_name() == "User"
+    assert dim.get_class_name() == "User"
     assert dim.get_hello_world() == "hello world"
