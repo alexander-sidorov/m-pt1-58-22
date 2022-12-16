c8c5b9bccb998e3c624820a8973190bfd1c179c4
EvgVavilov <785vvv@gmail.com>
2022-12-16 20:33:18 +0300

    add exercize-1

---
+++ hw/eugene_vavilov/lesson10/lesson_test.py
@@ -0,0 +1,15 @@
+from hw.eugene_vavilov.lesson10.lesson import User
+
+
+def test_01() -> None:
+    hw_text = "hello world"
+    petya = User("Петя")
+    vasya = User("Вася")
+
+    assert petya.get_name() == "Петя"
+    assert petya.get_class_name() == User.__name__
+    assert petya.get_hello_world() == hw_text
+
+    assert vasya.get_name() == "Вася"
+    assert vasya.get_class_name() == User.__name__
+    assert vasya.get_hello_world() == hw_text
