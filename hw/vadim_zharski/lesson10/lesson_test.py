fd3835012bf2adde02967babfe734257177a607e
vadim_zharski <vadimzharsky1990@gmail.com>
2022-12-16 20:49:23 +0300

    Vadim Zharski lesson 10

--- hw/vadim_zharski/lesson10/lesson_test.py
+++ hw/vadim_zharski/lesson10/lesson_test.py
@@ -8,4 +8,4 @@ def test_01() -> None:
     assert vasya.get_class_name() == User.__name__
     assert petya.get_hello_world() == "hello world"
     assert vasya.get_hello_world() == "hello world"
-    assert not petya == vasya
+    assert petya != vasya
