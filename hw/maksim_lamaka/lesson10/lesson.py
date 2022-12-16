cd45120396a703b0ba5b1007257e50aafd92ef63
maksim_lamaka <Lomanchioni@gmail.com>
2022-12-16 20:52:34 +0300

    zadanie1

--- hw/maksim_lamaka/lesson10/lesson.py
+++ hw/maksim_lamaka/lesson10/lesson.py
@@ -2,10 +2,11 @@ class User:
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def get_name(self):
+    def get_name(self)->str:
         return self.name
 
-    def get_class_name(self):
+    def get_class_name(self)->str:
         return User.__name__
 
-    def
+    def get_hello_world(self) -> str:
+        return "hello world"
--- hw/maksim_lamaka/lesson10/lesson_test.py
+++ hw/maksim_lamaka/lesson10/lesson_test.py
@@ -1,7 +1,16 @@
 from hw.maksim_lamaka.lesson10.lesson import User
 
 def test_01():
-    petya = User("Петя")
-    assert petya.get_name() == "Петя"
-    assert petya.get_class_name() == "User"
-    assert petya.get_hello_world() == "hello world"
+    hw_text = "hello world"
+    name = "Петя"
+    petya = User(name)
+    assert petya.get_name() == name
+    assert petya.get_class_name() == User.__name__
+    assert petya.get_hello_world() == hw_text
+
+    name = "Вася"
+    vasya = User(name)
+    assert vasya.get_name() == name
+    assert vasya.get_class_name() == User.__name__
+    assert vasya.get_hello_world() == hw_text
+
