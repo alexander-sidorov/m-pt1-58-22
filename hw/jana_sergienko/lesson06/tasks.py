e153203c497cb189c178001434e26c7409b41573
Sergienko95 <jana_monkey@mail.ru>
2022-12-16 10:54:24 +0300

    lesson_06

---
+++ hw/jana_sergienko/lesson06/tasks.py
@@ -0,0 +1,44 @@
+from typing import Sequence
+from typing import Any
+from math import sqrt
+
+from hw.alexander_sidorov.helpers import CITIES
+
+def task_01_boundary(sequence: Sequence) -> tuple:
+    return sequence[0] + sequence[-1]
+
+
+def task_02_expand(sequence: Sequence) -> Any:
+    return sequence[1:] * int(sequence[0])
+
+
+def task_03_hdist(seq1: Sequence, seq2: Sequence) -> int:
+    val = abs(len(seq1) - len(seq2))
+    zip_val = zip(seq1, seq2)
+    for i in zip_val:
+        if zip_val[0] != zip_val[1]:
+            val += 1
+        return val
+
+
+def task_04_cities(city: str) -> dict:
+    val = {}
+    for i in CITIES.keys():
+        val_1 = CITIES[city]
+        val_2 = CITIES[i]
+        x, y = (val_1[0] - val_2[0]) * 111, (val_1[1] - val_2[1]) * 65
+        distance = int((x ** 2 + y ** 2) ** 0.5)
+        val.update({i: distance})
+    return val
+
+
+def task_05_route(route: Sequence) -> float:
+    def distance(city1: tuple, city2: tuple) -> float:
+        value_0 = (city1[0] - city2[0]) * 111
+        value_1 = (city1[1] - city2[1]) * 65
+        return sqrt((value_0 ** 2) + (value_1 ** 2))
+    dist = float(0)
+    for town in route[1:]:
+        previous_town_cords = CITIES[route[route.index(town) - 1]]
+        dist += distance(CITIES[town], previous_town_cords)
+    return dist
---
+++ hw/jana_sergienko/lesson06/tasks_test.py
@@ -0,0 +1,32 @@
+from hw.jana_sergienko.lesson06.tasks import task_01_boundary
+from hw.jana_sergienko.lesson06.tasks import task_02_expand
+from hw.jana_sergienko.lesson06.tasks import task_03_hdist
+from hw.jana_sergienko.lesson06.tasks import task_04_cities
+from hw.jana_sergienko.lesson06.tasks import task_05_route
+
+
+def task_01_boundary_test() -> None:
+    assert task_01_boundary("ab") == ("a", "b")
+
+
+def task_02_expand() -> None:
+    assert task_02_expand([2, 3, 4]) == [3, 4, 3, 4]
+
+
+def task_03_hdist() -> None:
+    assert task_03_hdist("aaa", "aab") == 1
+    assert task_03_hdist("aaa", "aba") == 1
+    assert task_03_hdist("aaa", "baa") == 1
+    assert task_03_hdist("", "baa") == 3
+
+
+def task_04_cities() -> None:
+    distances = task_04_cities("Минск")
+    km = distances["Жодино"]
+    assert int(km) == 54
+
+
+def task_05_route() -> None:
+    km = task_05_route(("Минск", "Минск", "Жодино", "Жодино", "Минск"))
+    assert int(km) == 108
+    
