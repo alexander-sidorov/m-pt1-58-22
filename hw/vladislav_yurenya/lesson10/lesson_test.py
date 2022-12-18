6b10b1e4a606fedb29fcca3d815bc94a2dd43138
varfalomey777 <119004516+varfalomey777@users.noreply.github.com>
2022-12-18 15:07:55 +0300

    Vladislav Yurenya Lesson 10 (#62)

---
+++ hw/vladislav_yurenya/lesson10/lesson_test.py
@@ -0,0 +1,19 @@
+from hw.vladislav_yurenya.lesson10.lesson import Counter
+from hw.vladislav_yurenya.lesson10.lesson import User
+
+
+def test_01() -> None:
+    name01 = User("Zhora")
+    name02 = User("Вася")
+    assert name01.get_name() == "Zhora"
+    assert name01.get_class_name() == User.__name__
+    assert name01.get_hello_world() == "hello world"
+    assert name02.get_name() == "Вася"
+    assert name02.get_class_name() == User.__name__
+    assert name02.get_hello_world() == "hello world"
+
+
+def test_02() -> None:
+    count = Counter(-3, 3)
+    assert count.next() == -3
+    assert count.next() == -2
