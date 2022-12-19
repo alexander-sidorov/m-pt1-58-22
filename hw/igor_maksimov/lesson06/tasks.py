8fca9ce80b8c8b62a09375be9fa3c046f32fec12
Yigal Igor Maksimov <yigal.maksimov@gmail.com>
2022-12-19 20:26:57 +0300

    lesson 06

--- hw/igor_maksimov/lesson06/tasks.py
+++ hw/igor_maksimov/lesson06/tasks.py
@@ -1,38 +1,49 @@
 from typing import Any
-from math import sqrt
 from typing import Sequence
 
 from hw.alexander_sidorov.helpers import CITIES
 
 
-def task_01_boundary(suquence: Sequence) -> tuple:
-    return suquence[0], suquence[-1]
+def task_01_boundary(sequence: Sequence) -> tuple:
+    return tuple(sequence[0] + sequence[-1])
 
 
-def task_02_expand(suquence: Sequence) -> None:
-    return suquence[1:] * suquence[0]
+def task_02_expand(sequence: Sequence) -> Any:
+    return sequence[1:] * sequence[0]
 
 
 def task_03_hdist(seq1: Sequence, seq2: Sequence) -> int:
-    y = 0
-    res = [x for x in seq1 + seq2 if x not in seq1 or x not in seq2]
-    if len(res) > 0:
-        y = y + len(res)
-        return y
-    else:
-        return y
+    alt = 0
+    alt = abs(len(seq1) - len(seq2))
+    zip_alt = zip(seq1, seq2)
+    for i in zip_alt:
+        if i[0] != i[1]:
+            alt += 1
+    return alt
 
 
-def dist(*city) -> float:
-    new_city = [city]
-    if len(city) == 2 and city[0][0] < city[1][0]:
-        total_att = city[1][0] - city[0][0] * 111
-        total_lng = city[1][1] - city[0][1] * 65
-        return sqrt((total_att**2) + (total_lng**2))
-    elif len(city) == 2 and city[0][0] > city[1][0]:
-        total_att = city[0][0] - city[1][0] * 111
-        total_lng = city[0][1] - city[1][1] * 65
-        return sqrt((total_att**2) + (total_lng**2))
+def task_04_cities(city: str) -> dict:
+    total = {}
+    for key in CITIES.keys():
+        one = CITIES[city]
+        two = CITIES[key]
+        x, y = (one[0] - two[0]) * 111, (one[1] - two[1]) * 65
+        distance = int((x**2 + y**2) ** 0.5)
+        total.update({key: distance})
+    return total
 
 
-print(dist((52.534754, 24.984273), (53.132227, 26.017363), (12, 10)))
+def task_05_route(route: Sequence) -> int:
+    total = 0
+    value: Any = ()
+    for i in route:
+        value = value + CITIES[i]
+    first = value[0::2]  # type: tuple
+    sec = value[1::2]  # type: tuple
+    for ind in range(len(first) - 1):
+        lon, lat = (first[ind] - first[ind + 1]) * 111, (
+            sec[ind] - sec[ind + 1]
+        ) * 65
+        distance = int((lon**2 + lat**2) ** 0.5)
+        total += distance
+    return total
--- hw/igor_maksimov/lesson06/tasks_test.py
+++ hw/igor_maksimov/lesson06/tasks_test.py
@@ -1,6 +1,8 @@
 from hw.igor_maksimov.lesson06.tasks import task_01_boundary
 from hw.igor_maksimov.lesson06.tasks import task_02_expand
 from hw.igor_maksimov.lesson06.tasks import task_03_hdist
+from hw.igor_maksimov.lesson06.tasks import task_04_cities
+from hw.igor_maksimov.lesson06.tasks import task_05_route
 
 
 def task_01_boundary_test() -> None:
@@ -16,3 +18,14 @@ def task_03_hdist_test() -> None:
     assert task_03_hdist("aaa", "aba") == 1
     assert task_03_hdist("aaa", "baa") == 1
     assert task_03_hdist("", "baa") == 3
+
+
+def test_task_04_cities() -> None:
+    distances = task_04_cities("Минск")
+    km = distances["Жодино"]
+    assert int(km) == 54
+
+
+def test_task_05_route() -> None:
+    km = task_05_route(("Минск", "Минск", "Жодино", "Жодино", "Минск"))
+    assert int(km) == 108
