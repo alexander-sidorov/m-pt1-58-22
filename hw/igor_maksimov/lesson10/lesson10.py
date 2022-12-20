33a20a4502e7289194b6db708bc30c6cb6470721
Yigal Igor Maksimov <yigal.maksimov@gmail.com>
2022-12-20 15:38:40 +0300

    fixed lesson 10

--- hw/igor_maksimov/lesson10/lesson10.py
+++ hw/igor_maksimov/lesson10/lesson10.py
@@ -1,19 +1,19 @@
 class User:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
 
-    def get_name(self):
+    def get_name(self) -> str:
         return self.name
 
-    def get_class_name(self):
+    def get_class_name(self) -> str:
         return User.__name__
 
-    def get_hello_world(self):
+    def get_hello_world(self) -> str:
         return "hello world"
 
 
 class Counter:
-    def __init__(self, start, end):
+    def __init__(self, start: int, end: int):
         self.start = start
         self.end = end
         self.current = self.start
