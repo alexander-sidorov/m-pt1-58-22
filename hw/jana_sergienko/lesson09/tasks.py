d5d730c3152ca834b0be85ef6f595069838c6dd5
Sergienko95 <jana_monkey@mail.ru>
2022-12-20 09:22:03 +0300

    changes task01, typing

--- hw/jana_sergienko/lesson09/tasks.py
+++ hw/jana_sergienko/lesson09/tasks.py
@@ -1,5 +1,9 @@
-def task_01_do_twice(func) -> None:
-    def wrapper(*args, **kwargs) -> None:
+from typing import Any
+from typing import Callable
+
+
+def task_01_do_twice(func: Callable) -> Callable:
+    def wrapper(*args: Any, **kwargs: Any) -> None:
         func(*args, **kwargs)
         func(*args, **kwargs)
     return wrapper
