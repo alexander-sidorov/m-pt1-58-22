49a70e86953c3de8019a8667b7ef96b53c38209b
Mikhailiuk-Dmitriy <mds5@mail.ru>
2022-12-21 19:33:50 +0300

    added str mothod

--- hw/dmitry_mihkailiuk/lesson10/lesson_test.py
+++ hw/dmitry_mihkailiuk/lesson10/lesson_test.py
@@ -4,7 +4,7 @@ from hw.dmitry_mihkailiuk.lesson10.lesson import User
 
 def test_01() -> None:
     dim = User("Dim")
-    assert dim.get_name() == "Dim"
+    assert str(dim) == "Dim"
     assert dim.get_class_name() == "User"
     assert dim.get_hello_world() == "hello world"
 
