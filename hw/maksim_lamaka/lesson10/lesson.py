46559a3e604cbeae209c5034791a5e744cf1bbe2
maksim_lamaka <Lomanchioni@gmail.com>
2022-12-21 19:00:41 +0300

    lesso10

--- hw/maksim_lamaka/lesson10/lesson.py
+++ hw/maksim_lamaka/lesson10/lesson.py
@@ -1,11 +1,25 @@
+class Counter:
+    def __init__(self, start: int, stop: int):
+        self.start = start
+        self.stop = stop
+        self.current = self.start
+
+    def next(self) -> int:  # noqa: A003
+        if self.current > self.stop:
+            return self.stop
+
+        result, self.current = self.current, self.current + 1
+        return result
+
+
 class User:
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str):
         self.name = name
 
-    def get_name(self)->str:
+    def get_name(self) -> str:
         return self.name
 
-    def get_class_name(self)->str:
+    def get_class_name(self) -> str:
         return User.__name__
 
     def get_hello_world(self) -> str:
--- hw/maksim_lamaka/lesson10/lesson_test.py
+++ hw/maksim_lamaka/lesson10/lesson_test.py
@@ -1,16 +1,27 @@
+from hw.maksim_lamaka.lesson10.lesson import Counter
 from hw.maksim_lamaka.lesson10.lesson import User
 
-def test_01():
+def test_01() -> None:
     hw_text = "hello world"
-    name = "Петя"
-    petya = User(name)
-    assert petya.get_name() == name
+    petya = User("Петя")
+    vasya = User("Вася")
+
+    assert petya.get_name() == "Петя"
     assert petya.get_class_name() == User.__name__
     assert petya.get_hello_world() == hw_text
 
-    name = "Вася"
-    vasya = User(name)
-    assert vasya.get_name() == name
+    assert vasya.get_name() == "Вася"
     assert vasya.get_class_name() == User.__name__
     assert vasya.get_hello_world() == hw_text
 
+
+def test_02() -> None:
+    r1 = 1
+    ctr1 = Counter(0, r1)
+
+    r2 = 2
+    ctr2 = Counter(0, r2)
+
+    assert [ctr1.next() for _ in range(r1 + 1)] == [0, 1]
+    assert [ctr2.next() for _ in range(r2 + 1)] == [0, 1, 2]
+    assert [ctr2.next() for _ in range(r2 + 1)] == [2, 2, 2]
