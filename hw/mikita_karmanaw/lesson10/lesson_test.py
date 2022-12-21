c73043439cace36aeb09ac56cec75ec4f3b5930e
mikitakarman <mikita.karmanaw@gmail.com>
2022-12-21 19:38:46 +0300

    task3-1-2

--- hw/mikita_karmanaw/lesson10/lesson_test.py
+++ hw/mikita_karmanaw/lesson10/lesson_test.py
@@ -5,9 +5,9 @@ from hw.mikita_karmanaw.lesson10.lesson import User
 def test_01() -> None:
     alice = User("Alice")
     assert alice.get_user_name() == "Alice"
-    assert alice.get_class_name() == "Alice"
+    assert alice.get_class_name() == "User"
     assert alice.get_hello_world() == "hello world"
-    assert str(alice) == "User"
+    assert str(alice) == "Alice"
     bob = User("Bob")
     assert bob.get_user_name() == "Bob"
     assert bob.get_class_name() == "User"
