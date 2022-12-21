ec951831a3517c0e6dd5960103340ff4c8d4bcec
Alexander Sidorov <a375296648709@gmail.com>
2022-12-21 21:03:44 +0300

    task 5 (test)

--- hw/alexander_sidorov/lesson10/lesson_test.py
+++ hw/alexander_sidorov/lesson10/lesson_test.py
@@ -1,3 +1,5 @@
+import json
+
 from hw.alexander_sidorov.lesson10.lesson import Counter
 from hw.alexander_sidorov.lesson10.lesson import User
 
@@ -20,3 +22,11 @@ def test_task_02() -> None:
     assert list(Counter(0, 2)) == [0, 1, 2]
     assert tuple(Counter(1, 3)) == (1, 2, 3)
     assert set(Counter(-1, 1)) == {-1, 0, 1}
+
+
+def test_task_05() -> None:
+    petya = User("P")
+    js = petya.to_json()
+
+    assert js == '{"name": "P"}'
+    assert json.loads(js) == {"name": "P"}
