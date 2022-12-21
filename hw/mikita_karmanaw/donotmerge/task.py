44546c61ef1dc80cba54fca364d350fcff1f249e
karmaxa <mikita.karmanaw@gmail.com>
2022-12-21 13:10:16 +0300

    do-not-merge-3

--- hw/mikita_karmanaw/donotmerge/task.py
+++ hw/mikita_karmanaw/donotmerge/task.py
@@ -1,11 +1,14 @@
+from typing import Callable
+
+
 def func() -> int:
     return -1
 
 
-def decor(func) -> None:
+def decor(function: Callable) -> None:
     def wrapper() -> None:
-        try:
-            func()
+        try:  # noqa: SIM105
+            function()
         except AssertionError:  # noqa: SIM105
             pass
         return None
