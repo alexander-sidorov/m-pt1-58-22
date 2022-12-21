86315e062217cf2f798320fb95f746bd83318774
Yigal Maksimov <yiga.maksimov@gmail.com>
2022-12-21 20:45:44 +0300

    lesson11 task 04

--- hw/igor_maksimov/lesson10/lesson10.py
+++ hw/igor_maksimov/lesson10/lesson10.py
@@ -23,12 +23,12 @@ class Counter:
         self.stop = stop
         self.current: Optional[int] = None
 
-    def next(self) -> int:  # noqa: A003
-        if self.current is None:
-            self.current = self.start
+    def __iter__(self) -> Counter:
+        return self
 
+    def __next__(self) -> int:  # noqa: A003
         if self.current > self.stop:
-            return self.stop
+            raise StopIteration
 
         result, self.current = self.current, self.current + 1
         return result
