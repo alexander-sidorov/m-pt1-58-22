e35b1061f87e89b1c095e245794d2ac493939f9e
Vladislav Yurenya <228322fsa@mail.ru>
2022-12-21 19:30:21 +0300

    lesson 11 task 3

--- hw/vladislav_yurenya/lesson10/lesson.py
+++ hw/vladislav_yurenya/lesson10/lesson.py
@@ -2,8 +2,8 @@ class User:
     def __init__(self, name: str):
         self.name = name
 
-    def get_name(self) -> str:
-        return self.name
+    def __str__(self) -> str:
+        return f"{self.name}"
 
     def get_class_name(self) -> str:
         return User.__name__
--- hw/vladislav_yurenya/lesson10/lesson_test.py
+++ hw/vladislav_yurenya/lesson10/lesson_test.py
@@ -5,10 +5,10 @@ from hw.vladislav_yurenya.lesson10.lesson import User
 def test_01() -> None:
     name01 = User("Zhora")
     name02 = User("Вася")
-    assert name01.get_name() == "Zhora"
+    assert name01.__str__() == "Zhora"
     assert name01.get_class_name() == User.__name__
     assert name01.get_hello_world() == "hello world"
-    assert name02.get_name() == "Вася"
+    assert name02.__str__() == "Вася"
     assert name02.get_class_name() == User.__name__
     assert name02.get_hello_world() == "hello world"
 
