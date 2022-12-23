130bc55a7ca780ebb69f40e2eb0f1b58cb5cca0c
AliakseiTsiukhai <leha26061992@gmail.com>
2022-12-23 21:54:37 +0300

    lesson12, unfinish

--- hw/alexey_tyuhai/lesson12/lesson.py
+++ hw/alexey_tyuhai/lesson12/lesson.py
@@ -13,3 +13,16 @@ def task_01_urlsplit(url: str) -> dict:
         "query": url_cort.query,
         "fragment": url_cort.fragment,
     }
+
+class Request:
+    def __int__(self, url: str):
+        line = url.split("\n")
+        self.method, self.path, self.version = line[0].split(" ")
+        line = line[1::]
+        self.headers = {}
+        for i in line:
+            k, v = url.split(":")
+            self.headers[k] = v
+
+r = Request()
+print()
