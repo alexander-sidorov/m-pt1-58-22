7405d3a70d86c8d648811247760c93037506fa58
Yigal Maksimov <yiga.maksimov@gmail.com>
2022-12-23 21:30:11 +0300

    lesson 12 task 1

--- hw/igor_maksimov/lesson12/tasks.py
+++ hw/igor_maksimov/lesson12/tasks.py
@@ -1,31 +1,15 @@
+from urllib.parse import urlparse
 
 
-
-def get_schema(url:str)->dict:
-    lib={}
-    new_url = url  
-    schema = url.split("://")[0]
-
-    print(schema)
-    lib.update({'schema': schema})
- 
-    user = url.split(":")[1].replace("//","")
-    print(user)
-    lib.update({"user": user})
-
-    password = url.split(":")[2].split("@")[0]
-    lib.update({"password": password})
-  
-    host = url.split(":")[2].split("@")[1]
-    lib.update({"host": host})
-
-    port = url.split(":")[3].split("/")[0]
-    lib.update({"port":port})
-
-    res = url.split("/")[3]
-    lib.update({"resource": res})
-
-    print(lib)
-    return lib
-
-get_schema("http://gena:password@localhost:1234/resource")   
+def task_01_urlsplit(url: str) -> dict:
+    url_var = urlparse(url)
+    return {
+        "scheme": url_var.scheme,
+        "user": url_var.username,
+        "password": url_var.password,
+        "host": url_var.hostname,
+        "port": url_var.port,
+        "path": url_var.path,
+        "query": url_var.query,
+        "fragment": url_var.fragment,
+    }
