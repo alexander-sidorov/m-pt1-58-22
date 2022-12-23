a5593053f18a0cfefd2d3fe628902516553e4b76
zhenya baki <zhenyabaki@gmail.com>
2022-12-23 19:53:18 +0300

    new branche

---
+++ hw/eugene_bakun/__init__.py
@@ -0,0 +1,7 @@
+def task_01_urlsplit(url: str) -> dict:
+
+    comps = task_01_urlsplit("http://user:password@host:1234/resourse")
+    assert comps("scheme") == "http"
+    assert comps("userinfo") == "user:password"
+    assert comps("host") == "host"
+    assert comps("port") == "1234"
