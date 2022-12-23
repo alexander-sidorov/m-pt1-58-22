63953a98f7d9c33d29088c98682da71e3c9022bc
eugene_lubimov <you@example.com>
2022-12-23 21:48:37 +0300

    added class request

--- hw/eugene_lubimov/lesson12/lesson12.py
+++ hw/eugene_lubimov/lesson12/lesson12.py
@@ -13,3 +13,41 @@ def pars_url(url: str) -> dict:
         "query": pars.query,
         "fragment": pars.fragment,
     }
+
+
+class Request:
+    def __init__(self, req: str) -> None:
+        self.req = req.split("\n")
+
+    def method(self) -> str:
+        return self.req[0].split()[0]
+
+    def path(self) -> str:
+        return self.req[0].split()[1]
+
+    def version(self) -> str:
+        return self.req[0].split()[2]
+
+    def headers(self) -> dict:
+        self.headers = {}
+        for head in self.req[1:-3]:
+            head = head.split(": ")
+            self.headers[head[0]] = head[1]
+        return self.headers
+
+    def body(self) -> str:
+        return self.req[-1]
+
+
+req = """HEAD /docs/python_3/library/urllib.parse.html HTTP/1.1
+Accept: */*
+Accept-Encoding: gzip, deflate
+Connection: keep-alive
+Host: digitology.tech
+User-Agent: HTTPie/3.2.1
+
+
+"""
+
+for i in req.split('\n')[1:-3]:
+    print(i)
--- hw/eugene_lubimov/lesson12/test_lesson12.py
+++ hw/eugene_lubimov/lesson12/test_lesson12.py
@@ -8,3 +8,23 @@ def test_pars_url() -> None:
     my_d = les.pars_url(url)
 
     assert my_d["scheme"] == "https"
+
+
+def test_request() -> None:
+
+    req = """HEAD /docs/python_3/library/urllib.parse.html HTTP/1.1
+Accept: */*
+Accept-Encoding: gzip, deflate
+Connection: keep-alive
+Host: digitology.tech
+User-Agent: HTTPie/3.2.1
+
+
+"""
+    req1 = les.Request(req)
+
+    assert req1.method() == "HEAD"
+    assert req1.path() == "/docs/python_3/library/urllib.parse.html"
+    assert req1.version() == "HTTP/1.1"
+    assert req1.headers()["Accept"] == "*/*"
+    assert req1.body() == ""
