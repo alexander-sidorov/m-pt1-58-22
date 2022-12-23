6ba9fe91579b7011fcba33d6edc867085475da79
zhenya baki <zhenyabaki@gmail.com>
2022-12-23 21:53:09 +0300

    'eugene-bakun-lesson-12'

--- hw/eugene_bakun/lesson_10/lesson.py
+++ hw/eugene_bakun/lesson_10/lesson.py
@@ -1,27 +1,34 @@
+from typing import Optional
+
+
 class User:
- def __init__(self, name: str):
+    pass
+
+    def __init__(self, name: str):
         self.name = name
 
- def get_name(self) -> str:
-      return self.name
+    def get_name(self) -> str:
+        return self.name
 
- def get_class_name(self) -> str:
-      return User.__name__
+    def get_class_name(self) -> str:
+        return User.__name__
 
- def get_hello_world(self) -> str:
-      return "hello world"
+    def get_hello_world(self) -> str:
+        return "hello world"
 
 
- def __init__(self, start: int, finish: int):
+class Counter:
+    def __init__(self, start: int, stop: int):
         self.start = start
-        self.finish = finish
-        self.current: int | None = None
-        self.current = self.start
+        self.stop = stop
+        self.current: Optional[int] = None
 
+    def next(self) -> int:  # noqa: A003
+        if self.current is None:
+            self.current = self.start
 
- def next(self) -> int:  # noqa: A003
-    if  self.current is None:
-        self.current = self.start
+        if self.current > self.stop:
+            return self.stop
 
-    if  self.current > self.stop:
-      return self.stop
+        result, self.current = self.current, self.current + 1
+        return result
