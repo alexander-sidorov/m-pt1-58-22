da33ac3faa6725273e9723ccdd9a39e6583ea337
zhenya baki <zhenyabaki@gmail.com>
2022-12-23 21:39:42 +0300

    'eugene-bakun-lesson-12'

--- hw/eugene_bakun/__init__.py
+++ hw/eugene_bakun/__init__.py
@@ -1,7 +1,1 @@
-def task_01_urlsplit(url: str) -> dict:
 
-    comps = task_01_urlsplit("http://user:password@host:1234/resourse")
-    assert comps:("scheme") == "http"
-    assert comps:("userinfo") == "user:password"
-    assert comps:("host") == "host"
-    assert comps:("port") == "1234"
