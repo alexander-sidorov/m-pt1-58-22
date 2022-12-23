16e516a6a72c6dfc302f65e9d250595112c3adeb
Mikhailiuk-Dmitriy <mds5@mail.ru>
2022-12-23 21:56:40 +0300

    lesson12-2

--- hw/dmitry_mihkailiuk/lesson12/lesson.py
+++ hw/dmitry_mihkailiuk/lesson12/lesson.py
@@ -12,3 +12,14 @@ def task_01_urlsplit(url: str) -> dict:
         "query": url_c.query,
         "fragment": url_c.fragment,
     }
+
+
+class Request:
+    def __init__(self, get: str):
+        line = get.split("\n")
+        self.method, self.path, self.version = line[0].split(" ")
+        line = line[1::]
+        self.headers = {}
+        for i in line:
+            key, value = i.split(":")
+            self.headers[key] = value
--- hw/dmitry_mihkailiuk/lesson12/lesson_test.py
+++ hw/dmitry_mihkailiuk/lesson12/lesson_test.py
@@ -1,3 +1,4 @@
+from hw.dmitry_mihkailiuk.lesson12.lesson import Request
 from hw.dmitry_mihkailiuk.lesson12.lesson import task_01_urlsplit
 
 url = "vcn://user:pass@localhost:8000/s/cree/n1?q=2#x"
@@ -12,4 +13,26 @@ def test_01_urlsplit() -> None:
         "port": 8000,
         "query": "q=2",
         "fragment": "x",
+    }
+
+
+def test_02() -> None:
+
+    st = """HEAD /docs/cli HTTP/1.1
+Accept: */*
+Accept-Encoding: gzip, deflate
+Connection: keep-alive
+Host: httpie.io
+User-Agent: HTTPie/3.2.1"""
+
+    request = Request(st)
+    assert request.method == "HEAD"
+    assert request.path == "/docs/cli"
+    assert request.version == "HTTP/1.1"
+    assert request.headers == {
+        "Accept": " */*",
+        "Accept-Encoding": " gzip, deflate",
+        "Connection": " keep-alive",
+        "Host": " httpie.io",
+        "User-Agent": " HTTPie/3.2.1",
     }
