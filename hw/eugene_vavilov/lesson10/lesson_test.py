055d869059953b12bebb252b73df877f4e715169
EvgVavilov <785vvv@gmail.com>
2022-12-24 17:00:30 +0300

    lesson-11-test_mod

--- hw/eugene_vavilov/lesson10/lesson_test.py
+++ hw/eugene_vavilov/lesson10/lesson_test.py
@@ -8,32 +8,19 @@ def test_01() -> None:
     hw_text = "hello world"
     petya = User("Петя")
     vasya = User("Вася")
-
     assert str(petya) == "Петя"
     assert petya.get_class_name() == User.__name__
     assert petya.get_hello_world() == hw_text
-
     assert str(vasya) == "Вася"
     assert vasya.get_class_name() == User.__name__
     assert vasya.get_hello_world() == hw_text
 
 
 def test_02() -> None:
-    counter = Counter(10, 20)
-    assert next(counter) == 10
-    assert next(counter) == 11
-    assert next(counter) == 12
-    assert next(counter) == 13
-    assert next(counter) == 14
-    assert next(counter) == 15
-    assert next(counter) == 16
-    assert next(counter) == 17
-    assert next(counter) == 18
-    assert next(counter) == 19
-    assert next(counter) == 20
+    assert list(Counter(0, 3)) == [0, 1, 2, 3]
 
 
-def test_task_05() -> None:
+def test_05() -> None:
     petya = User("p")
     js = petya.to_json()
     assert js == '{"name": "p"}'
