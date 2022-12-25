3c4c15fb55852a1335ff9dc6408876cad0d3f51d
Mikhailiuk-Dmitriy <dmitriymikhailiuk@gmail.com>
2022-12-25 20:44:07 +0300

    lesson12-3

--- hw/dmitry_mihkailiuk/lesson12/lesson.py
+++ hw/dmitry_mihkailiuk/lesson12/lesson.py
@@ -1,25 +1,89 @@
-from urllib.parse import urlparse
+import json
+from typing import Any
 
 
-def task_01_urlsplit(url: str) -> dict:
-    url_c = urlparse(url)
-    return {
-        "schema": url_c.scheme,
-        "user": url_c.username,
-        "password": url_c.password,
-        "host": url_c.hostname,
-        "port": url_c.port,
-        "query": url_c.query,
-        "fragment": url_c.fragment,
-    }
+class Url:
+    def __init__(self, url: str):
+        self.scheme = None
+        self.username = None
+        self.password = None
+        self.host = None
+        self.port = None
+        self.path = None
+        self.query = None
+        self.fragment = None
+
+        self.scheme, other = url.split("://")
+
+        if "@" in other:
+            us_ps, other = other.split("@")
+            if ":" in us_ps:
+                self.username, self.password = us_ps.split(":")
+            else:
+                self.username = us_ps
+
+        if "#" in other:
+            other, self.fragment = other.rsplit("#")
+            if "?" in other:
+                other, self.query = other.rsplit("?")
+        elif "?" in other:
+            other, self.query = other.rsplit("?")
+
+        if "/" in other:
+            ho_po, path = other.split("/", 1)
+            self.path = "/" + path
+            if ":" in ho_po:
+                self.host, port = ho_po.split(":")
+                self.port = int(port)
+            else:
+                self.host = ho_po
+        elif ":" in other:
+            self.host, port = other.split(":")
+            self.port = int(port)
+        else:
+            self.host = other
 
 
-class Request:
+class HttpRequest:
     def __init__(self, get: str):
-        line = get.split("\n")
-        self.method, self.path, self.version = line[0].split(" ")
-        line = line[1::]
+        line, self.body = get.split("\n\n", 1)
+        self.body = self.body.strip()
+        line_list = line.split("\n")
+        self.method, self.path, self.http_version = line_list[0].split(" ")
+        line_list = line_list[1:]
         self.headers = {}
-        for i in line:
-            key, value = i.split(":")
-            self.headers[key] = value
+        for i in line_list:
+            key, value = i.strip().split(":", 1)
+            self.headers[key] = value.strip()
+        if not self.body:
+            self.body = None
+
+
+class HttpResponse:
+    def __init__(self, get: str):
+        line, self.body = get.split("\n\n", 1)
+        self.body = self.body.strip()
+        line_list = line.split("\n")
+        self.http_version, other = line_list[0].split(" ", 1)
+        status_code, self.reason = other.split(" ", 1)
+        self.status_code = int(status_code)
+        line_list = line_list[1:]
+        self.headers = {}
+        for i in line_list:
+            key, value = i.strip().split(":", 1)
+            if key == "Content-Length":
+                self.headers[key] = int(value)
+            else:
+                self.headers[key] = value.strip()
+
+    def is_valid(self) -> bool:
+        return self.headers["Content-Length"] == len(self.body)
+
+    def json(self) -> Any:
+        if self.headers["Content-Type"] == "application/json":
+            try:
+                return json.loads(self.body)
+            except json.decoder.JSONDecodeError:
+                return None
+        else:
+            return None
--- hw/dmitry_mihkailiuk/lesson12/lesson_test.py
+++ hw/dmitry_mihkailiuk/lesson12/lesson_test.py
@@ -1,38 +1,190 @@
-from hw.dmitry_mihkailiuk.lesson12.lesson import Request
-from hw.dmitry_mihkailiuk.lesson12.lesson import task_01_urlsplit
+from hw.dmitry_mihkailiuk.lesson12.lesson import HttpRequest
+from hw.dmitry_mihkailiuk.lesson12.lesson import HttpResponse
+from hw.dmitry_mihkailiuk.lesson12.lesson import Url
 
-url = "vcn://user:pass@localhost:8000/s/cree/n1?q=2#x"
+
+def test_url() -> None:
+    url = Url("http://google.com")
+    assert url.scheme == "http"
+    assert url.username is None
+    assert url.password is None
+    assert url.host == "google.com"
+    assert url.port is None
+    assert url.path is None
+    assert url.query is None
+    assert url.fragment is None
+
+    url = Url("postgresql://u:p@db:5432/dbname?opt=1&xyz=2#f")
+    assert url.scheme == "postgresql"
+    assert url.username == "u"
+    assert url.password == "p"  # noqa: S105
+    assert url.host == "db"
+    assert url.port == 5432
+    assert url.path == "/dbname"
+    assert url.query == "opt=1&xyz=2"
+    assert url.fragment == "f"
+
+    url = Url("vnc://user@host?query=q")
+    assert url.scheme == "vnc"
+    assert url.username == "user"
+    assert url.password is None
+    assert url.host == "host"
+    assert url.port is None
+    assert url.path is None
+    assert url.query == "query=q"
+    assert url.fragment is None
+
+    url = Url("vnc://user@host/?query=q")
+    assert url.scheme == "vnc"
+    assert url.username == "user"
+    assert url.password is None
+    assert url.host == "host"
+    assert url.port is None
+    assert url.path == "/"
+    assert url.query == "query=q"
+    assert url.fragment is None
+
+    url = Url("ftp://a.b.c.host/p/a/t/h#f")
+    assert url.scheme == "ftp"
+    assert url.username is None
+    assert url.password is None
+    assert url.host == "a.b.c.host"
+    assert url.port is None
+    assert url.path == "/p/a/t/h"
+    assert url.query is None
+    assert url.fragment == "f"
+
+    url = Url("vcn://user:pass@localhost:8000/s/cree/n1?q=2#x")
+    assert url.scheme == "vcn"
+    assert url.username == "user"
+    assert url.password == "pass"  # noqa: S105
+    assert url.host == "localhost"
+    assert url.port == 8000
+    assert url.path == "/s/cree/n1"
+    assert url.query == "q=2"
+    assert url.fragment == "x"
+
+    url = Url("http://google.com:8000")
+    assert url.scheme == "http"
+    assert url.username is None
+    assert url.password is None
+    assert url.host == "google.com"
+    assert url.port == 8000
+    assert url.path is None
+    assert url.query is None
+    assert url.fragment is None
 
 
-def test_01_urlsplit() -> None:
-    assert task_01_urlsplit(url) == {
-        "schema": "vcn",
-        "user": "user",
-        "password": "pass",
-        "host": "localhost",
-        "port": 8000,
-        "query": "q=2",
-        "fragment": "x",
+def test_http_request() -> None:
+
+    message = """HEAD / HTTP/1.1
+    Accept: */*
+    Accept-Encoding: gzip, deflate
+    Connection: keep-alive
+    Host: github.com
+    User-Agent: HTTPie/3.2.1
+
+    """
+
+    req = HttpRequest(message)
+
+    assert req.method == "HEAD"
+    assert req.path == "/"
+    assert req.http_version == "HTTP/1.1"
+    assert req.headers == {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate",
+        "Connection": "keep-alive",
+        "Host": "github.com",
+        "User-Agent": "HTTPie/3.2.1",
+    }
+    assert req.body is None
+
+    message = """HEAD /docs/cli HTTP/1.1
+    Accept: */*
+    Accept-Encoding: gzip, deflate
+    Connection: keep-alive
+    Host: httpie.io
+    User-Agent: HTTPie/3.2.1
+
+    <body>
+    """
+
+    req = HttpRequest(message)
+
+    assert req.method == "HEAD"
+    assert req.path == "/docs/cli"
+    assert req.http_version == "HTTP/1.1"
+    assert req.headers == {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate",
+        "Connection": "keep-alive",
+        "Host": "httpie.io",
+        "User-Agent": "HTTPie/3.2.1",
     }
 
+    assert req.body == "<body>"
 
-def test_02() -> None:
 
-    st = """HEAD /docs/cli HTTP/1.1
-Accept: */*
-Accept-Encoding: gzip, deflate
-Connection: keep-alive
-Host: httpie.io
-User-Agent: HTTPie/3.2.1"""
+def test_http_response() -> None:
+    message = """HTTP/1.1 404 NOT FOUND
+    Content-Length: 48
+    Content-Type: application/json
+    Server: gunicorn/19.9.0
 
-    request = Request(st)
-    assert request.method == "HEAD"
-    assert request.path == "/docs/cli"
-    assert request.version == "HTTP/1.1"
-    assert request.headers == {
-        "Accept": " */*",
-        "Accept-Encoding": " gzip, deflate",
-        "Connection": " keep-alive",
-        "Host": " httpie.io",
-        "User-Agent": " HTTPie/3.2.1",
+    {"status_code": 404, "description": "no access"}
+    """
+
+    resp = HttpResponse(message)
+
+    assert resp.status_code == 404
+    assert resp.reason == "NOT FOUND"
+    assert resp.http_version == "HTTP/1.1"
+    assert resp.headers == {
+        "Content-Length": 48,
+        "Content-Type": "application/json",
+        "Server": "gunicorn/19.9.0",
     }
+    assert resp.body == '{"status_code": 404, "description": "no access"}'
+    assert resp.is_valid()
+    assert resp.json() == {"status_code": 404, "description": "no access"}
+
+    message = """HTTP/1.1 404 NOT FOUND
+    Content-Length: 49
+    Content-Type: text/html
+    Server: gunicorn/19.9.0
+
+    {"status_code": 404, "description": "no access"}
+    """
+
+    resp = HttpResponse(message)
+
+    assert not resp.is_valid()
+    assert resp.json() is None
+
+    message = """HTTP/1.1 404 NOT FOUND
+    Content-Length: 48
+    Content-Type: text/html
+    Server: gunicorn/19.9.0
+
+    {"status_code": 404, "description": "no access"}
+    """
+
+    resp = HttpResponse(message)
+
+    assert resp.is_valid()
+    assert resp.json() is None
+
+    message = """HTTP/1.1 404 NOT FOUND
+       Content-Length: 48
+       Content-Type: application/json
+       Server: gunicorn/19.9.0
+
+       "status_code": 404, "description": "no access"
+       """
+
+    resp = HttpResponse(message)
+
+    assert resp.body == '"status_code": 404, "description": "no access"'
+    assert not resp.is_valid()
+    assert resp.json() is None
