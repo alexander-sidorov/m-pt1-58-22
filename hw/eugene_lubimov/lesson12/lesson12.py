d6a7907c06bb63cb01acaec4421879b18a0d9d6a
Yauheni_Liubimov <0.5TELOO@gmail.com>
2022-12-25 22:21:34 +0300

    fixed

--- hw/eugene_lubimov/lesson12/lesson12.py
+++ hw/eugene_lubimov/lesson12/lesson12.py
@@ -1,36 +1,43 @@
 import json
+from typing import Any
 
 
 class Url:
-    def __init__(self, url: str) -> None:  # noqa: CCR001
+    def __init__(self, url: str) -> None:
+        my_dict = dict.fromkeys(
+            (
+                "sheme",
+                "username",
+                "password",
+                "fragment",
+                "query",
+                "path",
+                "host",
+                "port",
+            )
+        )
+        self.__dict__.update(my_dict)
         self.scheme, other = url.split("://")
+
         if "@" in other:
-            userinfo, other = other.split("@")
-            if ":" in userinfo:
-                self.username, self.password = userinfo.split(":")
+            self.userinfo, other = other.split("@")
+            if ":" in self.userinfo:
+                self.username, self.password = self.userinfo.split(":")
             else:
-                self.username, self.password = userinfo, None
-        else:
-            self.username, self.password = None, None
+                self.username = self.userinfo
+
         if "#" in other:
             other, self.fragment = other.split("#")
-        else:
-            self.fragment = None
         if "?" in other:
             other, self.query = other.split("?")
-
-        else:
-            self.query = None
         if "/" in other:
             ind = other.index("/")
             self.path, other = other[ind:], other[:ind]
-        else:
-            self.path = None
         if ":" in other:
             self.host, port = other.split(":")
             self.port = int(port)
         else:
-            self.host, self.port = other, None
+            self.host: str = other
 
 
 class HttpRequest:
@@ -45,7 +52,7 @@ class HttpRequest:
         for head in self.req[1:end]:
             header, value = head.split(": ")
             if value.isdigit():
-                value = int(value)
+                value: int = int(value)
             self.__headers_dict[header] = value
         self.__body = self.req[-2] if self.req[-2] else None
 
@@ -83,13 +90,13 @@ class HttpResponse(HttpRequest):
         return int(self.path)
 
     @property
-    def reason(self) -> str:
+    def reason(self) -> Any:
         return self._HttpRequest__http_version.title()
 
-    def is_valid(self) -> bool:
+    def is_valid(self) -> Any:
         return self.headers["Content-Length"] == len(self.body)
 
-    def json(self) -> dict | None:
+    def json(self) -> str | None:
         if self.headers["Content-Type"] == "application/json":
             try:
                 self.__json = json.loads(self.body)
