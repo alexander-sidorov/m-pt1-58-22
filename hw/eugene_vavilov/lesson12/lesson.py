dde1a2d857f70cf603b8bdab713e92c90cd9ee12
EvgVavilov <785vvv@gmail.com>
2022-12-26 15:39:00 +0300

    add task 2 + task 3

--- hw/eugene_vavilov/lesson12/lesson.py
+++ hw/eugene_vavilov/lesson12/lesson.py
@@ -1,55 +1,102 @@
-def task_01_urlsplit(url: str) -> dict:
-    schema = {}
-    separators = ["://", ":", "@", ":", "/", "?", "#"]
-    syntax_name = [
-        "scheme",
-        "user",
-        "password",
-        "host",
-        "port",
-        "path",
-        "query",
-        "fragment",
-    ]
+import json
+from typing import Any
+from urllib.parse import urlparse
 
-    ditail = url.split(separators[0], maxsplit=1)
-    schema[syntax_name[0]] = ditail[0]
-    url = ditail[1]
-    last_separator = 3
 
-    if "@" in url:
-        ditail = url.split(separators[1], maxsplit=1)
-        schema[syntax_name[1]] = ditail[0]
-        url = ditail[1]
+class Url:
+    def __init__(self, url: Any) -> None:
+        url = urlparse(url)
+        self.scheme = url.scheme
+        self.username = url.username
+        self.password = url.password
+        self.host = url.hostname
+        self.port = url.port
+        if url.path and len(url.path) > 0:
+            self.path = url.path
+        else:
+            self.path = None
+        if url.query and len(url.query) > 0:
+            self.query = url.query
+        else:
+            self.query = None
+        if url.fragment and len(url.fragment) > 0:
+            self.fragment = url.fragment
+        else:
+            self.fragment = None
 
-        ditail = url.split(separators[2], maxsplit=1)
-        schema[syntax_name[2]] = ditail[0]
-        url = ditail[1]
-        last_separator = 3
-    if ":" in url:
-        ditail = url.split(separators[3], maxsplit=1)
-        schema[syntax_name[3]] = ditail[0]
-        url = ditail[1]
-        last_separator = 4
 
-    if "/" in url:
-        ditail = url.split(separators[4], maxsplit=1)
-        schema[syntax_name[4]] = ditail[0]
-        url = ditail[1]
-        last_separator = 5
+class HttpRequest:
+    def __init__(self, request: str) -> None:
 
-    if "?" in url:
-        ditail = url.split(separators[5], maxsplit=1)
-        schema[syntax_name[5]] = ditail[0]
-        url = ditail[1]
-        last_separator = 6
+        request = request.strip()
 
-    if "#" in url:
-        ditail = url.split(separators[6], maxsplit=1)
-        schema[syntax_name[6]] = ditail[0]
-        url = ditail[1]
-        last_separator = 7
+        self.body: None = None
 
-    schema[syntax_name[last_separator]] = url
+        methed_end = request.find(" ")
+        self.method: str = (request[0:methed_end]).strip()
 
-    return schema
+        path_begin = request.find(" ")
+        path_end = request.find(" ", path_begin + 1)
+        self.path: str = (
+            request[path_begin + 1 : path_end]  # noqa E203
+        ).strip()
+
+        http_begin = request.find("HTTP")
+        self.http_version: str = (
+            request[http_begin : (http_begin + 9)]  # noqa E203
+        ).strip("\n")
+
+        request = (request[http_begin + 8 :]).strip("\n")  # noqa E203
+
+        heads = request.split("\n")
+
+        self.headers: dict = {}
+        for _ in heads:
+            _ = _.split(":")
+            _[0] = _[0].replace(" ", "")
+            _[1] = _[1].replace(" ", "")
+            self.headers.update({_[0]: _[1]})
+
+
+class HttpResponse:
+    def __init__(self, response: str):
+
+        response = response.strip()
+
+        self.http_version: str = (response[0:8]).strip(" \n")
+
+        status_begin = response.find(" ")
+        status_end = response.find(" ", status_begin + 1)
+        self.status_code: int = int(
+            (response[status_begin + 1 : status_end]).strip(" \n")  # noqa E203
+        )
+
+        reason_end = response.find("\n", status_end)
+        self.reason: str = (response[status_end:reason_end]).strip(" \n")
+
+        body_start = response.find("{")
+        headers = (response[reason_end:body_start]).strip("\n ")
+
+        headers_list = headers.split("\n")
+        self.headers = {}
+        for _ in headers_list:
+            _ = _.split(":")
+            _[0] = _[0].replace(" ", "")
+            _[1] = _[1].replace(" ", "")
+            try:  # noqa SIM105
+                _[1] = int(_[1])
+            except ValueError:
+                pass
+            self.headers.update({_[0]: _[1]})
+
+        body_end = response.find("}")
+        self.body = response[body_start : body_end + 1]  # noqa E203
+
+    def is_valid(self) -> Any:
+        return len(self.body) == self.headers["Content-Length"]
+
+    def json(self) -> Any:
+        if "json" in self.headers["Content-Type"]:
+            body = json.loads(self.body)
+            return body
+        return None
--- hw/eugene_vavilov/lesson12/lesson_test.py
+++ hw/eugene_vavilov/lesson12/lesson_test.py
@@ -1,16 +1,125 @@
-from hw.eugene_vavilov.lesson12.lesson import task_01_urlsplit
+from hw.eugene_vavilov.lesson12.lesson import HttpRequest
+from hw.eugene_vavilov.lesson12.lesson import HttpResponse
+from hw.eugene_vavilov.lesson12.lesson import Url
 
 
 def test_task_1() -> None:
-    schema = task_01_urlsplit("vnc://user:pass@localhost:8000/s/cree/n1?q=2#x")
-    assert schema["scheme"] == "vnc"
-    assert schema["user"] == "user"
-    assert schema["password"] == "pass"
-    assert schema["host"] == "localhost"
-    assert schema["port"] == "8000"
-    assert schema["path"] == "s/cree/n1"
-    assert schema["query"] == "q=2"
-    assert schema["fragment"] == "x"
-    schema = task_01_urlsplit("https://github.com")
-    assert schema["scheme"] == "https"
-    assert schema["host"] == "github.com"
+    url = Url("http://google.com")
+    assert url.scheme == "http"
+    assert url.username is None
+    assert url.password is None
+    assert url.host == "google.com"
+    assert url.port is None
+    assert url.path is None  # в этом случае можно "" или "/"
+    assert url.query is None
+    assert url.fragment is None
+
+    url = Url("postgresql://u:p@db:5432/dbname?opt=1&xyz=2#f")
+    assert url.scheme == "postgresql"
+    assert url.username == "u"
+    assert url.password == "p"  # noqa S105
+    assert url.host == "db"
+    assert url.port == 5432
+    assert url.path == "/dbname"
+    assert url.query == "opt=1&xyz=2"
+    assert url.fragment == "f"
+
+    url = Url("vnc://user@host?query=q")
+    assert url.scheme == "vnc"
+    assert url.username == "user"
+    assert url.password is None
+    assert url.host == "host"
+    assert url.port is None
+    assert url.path is None
+    assert url.query == "query=q"
+    assert url.fragment is None
+
+    url = Url("vnc://user@host/?query=q")
+    assert url.scheme == "vnc"
+    assert url.username == "user"
+    assert url.password is None
+    assert url.host == "host"
+    assert url.port is None
+    assert url.path == "/"
+    assert url.query == "query=q"
+    assert url.fragment is None
+
+    url = Url("ftp://a.b.c.host/p/a/t/h?query=q&f=f#f")
+    assert url.scheme == "ftp"
+    assert url.username is None
+    assert url.password is None
+    assert url.host == "a.b.c.host"
+    assert url.port is None
+    assert url.path == "/p/a/t/h"
+    assert url.query == "query=q&f=f"
+    assert url.fragment == "f"
+
+
+def test_task_2() -> None:
+    req = HttpRequest(
+        """HEAD / HTTP/1.1
+    Accept: */*
+    Accept-Encoding: gzip, deflate
+    Connection: keep-alive
+    Host: github.com
+    User-Agent: HTTPie/3.2.1
+    """
+    )
+    assert req.method == "HEAD"
+    assert req.path == "/"
+    assert req.http_version == "HTTP/1.1"
+    assert req.headers == {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip,deflate",
+        "Connection": "keep-alive",
+        "Host": "github.com",
+        "User-Agent": "HTTPie/3.2.1",
+    }
+    assert req.body is None
+
+
+def test_task_3() -> None:
+    resp = HttpResponse(
+        """HTTP/1.1 404 NOT FOUND
+    Content-Length: 48
+    Content-Type: application/json
+    Server: gunicorn/19.9.0
+    {"status_code": 404, "description": "no access"}
+    """
+    )
+
+    assert resp.status_code == 404
+    assert resp.reason == "NOT FOUND"
+    assert resp.http_version == "HTTP/1.1"
+    assert resp.headers == {
+        "Content-Length": 48,
+        "Content-Type": "application/json",
+        "Server": "gunicorn/19.9.0",
+    }
+    assert resp.body == '{"status_code": 404, "description": "no access"}'
+    assert resp.is_valid()
+    assert resp.json() == {"status_code": 404, "description": "no access"}
+
+    message = """HTTP/1.1 404 NOT FOUND
+    Content-Length: 49
+    Content-Type: text/html
+    Server: gunicorn/19.9.0
+    {"status_code": 404, "description": "no access"}
+    """
+
+    resp = HttpResponse(message)
+
+    assert not resp.is_valid()
+    assert resp.json() is None
+
+    message = """HTTP/1.1 404 NOT FOUND
+    Content-Length: 48
+    Content-Type: text/html
+    Server: gunicorn/19.9.0
+    {"status_code": 404, "description": "no access"}
+    """
+
+    resp = HttpResponse(message)
+
+    assert resp.is_valid()
+    assert resp.json() is None
