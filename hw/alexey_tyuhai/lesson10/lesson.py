cad4c422c3ed58e23c7ba04e830fc7a2760dd080
AliakseiTsiukhai <118986917+AliakseiTsiukhai@users.noreply.github.com>
2022-12-27 23:02:15 +0300

    Alexey Tuyhai Lesson 11 (#91)

--- hw/alexey_tyuhai/lesson10/lesson.py
+++ hw/alexey_tyuhai/lesson10/lesson.py
@@ -1,8 +1,13 @@
+import json
+from pathlib import Path
+from typing import Any
+
+
 class User:
     def __init__(self, name: str):
         self.name = name
 
-    def get_name(self) -> str:
+    def __str__(self) -> str:
         return self.name
 
     def get_class_name(self) -> str:
@@ -11,16 +16,27 @@ class User:
     def get_hello_world(self) -> str:
         return "hello world"
 
+    def to_json(self) -> str:
+        data = {"name": self.name}
+        return json.dumps(data)
+
+    def save_json(self, destination: str | Path) -> Any:
+        json_file = Path(destination)
+        with json_file.open("w") as stream:
+            json.dump({"name": self.name}, stream)
+
 
 class Counter:
     def __init__(self, start: int, end: int):
         self.start = start
         self.end = end
-        self.current = self.start
 
-    def next(self) -> int:  # noqa: A003
-        if self.current > self.end:
-            return self.end
+    def __iter__(self) -> "Counter":
+        return self
 
-        result, self.current = self.current, self.current + 1
-        return result
+    def __next__(self) -> int:
+        if self.start < self.end:
+            num = self.start
+            self.start += 1
+            return num
+        raise StopIteration
--- hw/alexey_tyuhai/lesson10/lesson_test.py
+++ hw/alexey_tyuhai/lesson10/lesson_test.py
@@ -1,3 +1,7 @@
+import json
+import tempfile
+from pathlib import Path
+
 from hw.alexey_tyuhai.lesson10.lesson import Counter
 from hw.alexey_tyuhai.lesson10.lesson import User
 
@@ -7,21 +11,45 @@ def test_01() -> None:
     petya = User("Петя")
     vasya = User("Вася")
 
-    assert petya.get_name() == "Петя"
+    assert str(petya) == "Петя"
     assert petya.get_class_name() == User.__name__
     assert petya.get_hello_world() == hw_text
 
-    assert vasya.get_name() == "Вася"
+    assert str(vasya) == "Вася"
     assert vasya.get_class_name() == User.__name__
     assert vasya.get_hello_world() == hw_text
 
 
 def test_02() -> None:
-    r1 = 1
+    r1 = 3
     ctr1 = Counter(0, r1)
-    r2 = 2
-    ctr2 = Counter(0, r2)
+    r2 = 4
+    ctr2 = Counter(1, r2)
 
-    assert [ctr1.next() for _ in range(r1 + 1)] == [0, 1]
-    assert [ctr2.next() for _ in range(r2 + 1)] == [0, 1, 2]
-    assert [ctr2.next() for _ in range(r2 + 1)] == [2, 2, 2]
+    assert next(ctr1) == 0
+    assert next(ctr1) == 1
+    assert next(ctr1) == 2
+    list_n = list(ctr2)
+    assert list_n == [1, 2, 3]
+
+
+def test_task_05() -> None:
+    petya = User("P")
+    js = petya.to_json()
+
+    assert js == '{"name": "P"}'
+    assert json.loads(js) == {"name": "P"}
+
+    with tempfile.TemporaryDirectory() as _tmpdir:
+        tmpdir = Path(_tmpdir)
+        assert tmpdir.is_dir()
+
+        json_file = tmpdir / "asd.json"
+        assert not json_file.is_file()
+
+        petya.save_json(json_file)
+        assert json_file.is_file()
+
+        with json_file.open("r") as stream:
+            jsonobj = json.load(stream)
+            assert jsonobj == {"name": "P"}
