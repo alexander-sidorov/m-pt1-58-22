b921793e193ab1b2e0554077ec23a3899198f718
VadimZharsky <vadimzharsky1990@gmail.com>
2022-12-28 11:53:22 +0300

    lesson_12

--- hw/vadim_zharski/lesson12/lesson.py
+++ hw/vadim_zharski/lesson12/lesson.py
@@ -1,59 +1,155 @@
+import json
+from typing import Any
+
+
 class UrlParser:
     def __init__(self, body: str):
         self.__body = body
+        self.__splitter()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.__body
 
-    def splitter(self) -> dict:
+    def __splitter(self) -> dict:  # CCR001
+        self.scheme = None
+        self.username = None
+        self.password = None
+        self.host = None
+        self.port = None
+        self.path = None
+        self.query = None
+        self.fragment = None
 
-        def inner(st: str, spl) -> tuple:
-            if spl in st:
-                return tuple(st.split(":"))
+        def inner(st: str, splitter: str) -> tuple:
+            if splitter in st:
+                return tuple(st.split(splitter))
             return st, None
 
-        self.__body.replace(" ", "")
-        tempstr = self.__body.replace('://', " ").replace("@", " ").replace("/", " /", 1).split(" ")
-        user, namepass = inner(tempstr[1], ":")
-        host, port = inner(tempstr[2], ":")
-        if "?" in tempstr[3]:
-            path, suffix = tempstr[3].split('?')
-            if '#' in suffix:
-                query, fragm = suffix.split('#')
-            else:
-                query, fragm = suffix, None
-        else:
-            path, query, fragm = tempstr[3], None, None
+        self.scheme, temp_str = self.__body.split("://")
+        if "?" in temp_str:
+            temp_str, query_frag = inner(temp_str, "?")
+            self.query, self.fragment = inner(query_frag, "#")
+        if "/" in temp_str:
+            temp_str, path = temp_str.replace("/", " /", 1).split(" ")
+            if path != "" and path != "/":
+                self.path = path
+        if "@" in temp_str:
+            user_pass, temp_str = temp_str.split("@")
+            self.username, self.password = inner(user_pass, ":")
+        self.host, port = inner(temp_str, ":")
+        if port:
+            self.port = int(port)
 
-        components = {
-            "scheme": tempstr[0],
-            "user": user,
-            "password": namepass,
-            "host": host,
-            "port": port,
-            "path": path,
-            "query": query,
-            "fragment": fragm,
+        components: dict = {
+            "scheme": self.scheme,
+            "username": self.username,
+            "password": self.password,
+            "host": self.host,
+            "port": self.port,
+            "path": self.path,
+            "query": self.query,
+            "fragment": self.fragment,
         }
         return components
 
 
-class Request:
+class HttpRequest:
+    def __init__(self, request_str: str):
 
-    def __init__(self, method, path: str, version: str, headers: dict, body: str):
-        self.__method = method
-        self.__path = path
-        self.__version = version
-        self.__headers = headers
-        self.__body = body
+        self.__request = request_str
+        self.__body_extractor()
+        self.__splitter()
+        self.__splitter_headers()
+
+    def __body_extractor(self) -> None:
+        self.body = None
+        self.__request, is_body = self.__request.split("\n\n", 1)
+        body_reform = is_body.strip()
+        if len(body_reform) > 0:
+            self.body = is_body.lstrip().rstrip()
+
+    def __splitter(self) -> None:
+        self.__request_list: list = self.__request.split("\n")
+        self.method, self.path, self.http_version = self.__request_list[
+            0
+        ].split(" ")
+        del self.__request_list[0]
+
+    def __splitter_headers(self) -> None:
+        self.headers: dict = {}
+        for pair in self.__request_list:
+            key, value = pair.split(":")
+            self.headers[key.strip()] = value.strip()
 
 
-# def main():
-#     test_url = UrlParser("http://user:password@host:1234/resourse")
-#     next_url = UrlParser("vnc://user:pass@localhost:8000/s/cree/n1?q=2#x")
-#     print(str(next_url))
-#     print(next_url.splitter())
-#
-#
-# if __name__ == "__main__":
-#     main()
+class HttpResponse:
+    def __init__(self, response: str):
+        self.__response = response
+        self.__body_extractor()
+        self.__response_list: list = self.__response.split('\n')
+        self.__splitter()
+        self.__splitter_headers()
+
+    def __body_extractor(self) -> None:
+        self.body = None
+        self.__response, is_body = self.__response.split("\n\n", 1)
+        body_reform = is_body.strip()
+        if len(body_reform) > 0:
+            self.body = is_body.lstrip().rstrip()
+
+    def __splitter(self) -> None:
+        self.http_version, status_code, self.reason = self.__response_list[0].split(' ', 2)
+        self.status_code = int(status_code)
+        del (self.__response_list[0])
+
+    def __splitter_headers(self) -> None:
+        self.headers: dict = {}
+        for pair in self.__response_list:
+            key, val = pair.split(':')
+            if "Content-Length" in key:
+                value = int(val)
+                self.headers[key.strip()] = value
+            else:
+                self.headers[key.strip()] = val.strip()
+
+    def is_valid(self) -> bool:
+        if self.body:
+            if self.headers["Content-Length"] == len(self.body):
+                return True
+        return False
+
+    def json(self) -> Any:
+        __json_obj = None
+        if self.is_valid() and self.headers["Content-Type"] == "application/json":
+            try:
+                __json_obj = json.loads(self.body)
+                return __json_obj
+            except json.decoder.JSONDecodeError:
+                return __json_obj
+        else:
+            return __json_obj
+
+
+def main():
+    message = """HTTP/1.1 404 NOT FOUND
+    Content-Length: 48
+    Content-Type: text/html
+    Server: gunicorn/19.9.0
+
+    {"status_code": 404, "description": "no access"}
+    """
+
+    resp = HttpResponse(message)
+
+    assert resp.is_valid()
+    assert resp.json() is None
+    printer(resp)
+    print(resp.json())
+
+
+def printer(obj: HttpResponse) -> None:
+    print(f"{obj.status_code}\n{obj.reason}\n{obj.http_version}\n{obj.headers}\n{obj.body}")
+
+
+if __name__ == "__main__":
+    main()
--- hw/vadim_zharski/lesson12/lesson_test.py
+++ hw/vadim_zharski/lesson12/lesson_test.py
@@ -2,15 +2,156 @@ import hw.vadim_zharski.lesson12.lesson as l_12
 
 
 def test_01() -> None:
-    test_url = l_12.UrlParser("http://user:password@host:1234/resourse")
-    next_url = l_12.UrlParser("vnc://user:pass@localhost:8000/s/cree/n1?q=2#x")
-    assert next_url.splitter() == {
-        "scheme": "vnc",
-        "user": "user",
-        "password": "pass",
-        "host": "localhost",
-        "port": "8000",
-        "path": "/s/cree/n1",
-        "query": "q=2",
-        "fragment": "x"
+    url = l_12.UrlParser("postgresql://u:p@db:5432/dbname?opt=1&xyz=2#f")
+    assert url.scheme == "postgresql"
+    assert url.username == "u"
+    assert url.password == "p"  # noqa S105
+    assert url.host == "db"
+    assert url.port == 5432
+    assert url.path == "/dbname"
+    assert url.query == "opt=1&xyz=2"
+    assert url.fragment == "f"
+
+    url = l_12.UrlParser("http://google.com")
+    assert str(url) == "http://google.com"
+    assert url.scheme == "http"
+    assert url.username is None
+    assert url.password is None
+    assert url.host == "google.com"
+    assert url.port is None
+    assert url.path is None
+    assert url.query is None
+    assert url.fragment is None
+
+    url = l_12.UrlParser("ftp://a.b.c.host/p/a/t/h?query=q&f=f#f")
+    assert url.scheme == "ftp"
+    assert url.username is None
+    assert url.password is None
+    assert url.host == "a.b.c.host"
+    assert url.port is None
+    assert url.path == "/p/a/t/h"
+    assert url.query == "query=q&f=f"
+    assert url.fragment == "f"
+
+    url = l_12.UrlParser("vnc://user@host/?query=q")
+    assert url.scheme == "vnc"
+    assert url.username == "user"
+    assert url.password is None
+    assert url.host == "host"
+    assert url.port is None
+    assert url.path is None
+    assert url.query == "query=q"
+    assert url.fragment is None
+
+    url = l_12.UrlParser("vnc://user@host?query=q")
+    assert url.scheme == "vnc"
+    assert url.username == "user"
+    assert url.password is None
+    assert url.host == "host"
+    assert url.port is None
+    assert url.path is None
+    assert url.query == "query=q"
+    assert url.fragment is None
+
+
+def test_02() -> None:
+    message = """HEAD / HTTP/1.1
+    Accept: */*
+    Accept-Encoding: gzip, deflate
+    Connection: keep-alive
+    Host: github.com
+    User-Agent: HTTPie/3.2.1
+
+    """
+
+    req = l_12.HttpRequest(message)
+
+    assert req.method == "HEAD"
+    assert req.path == "/"
+    assert req.http_version == "HTTP/1.1"
+    assert req.headers == {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate",
+        "Connection": "keep-alive",
+        "Host": "github.com",
+        "User-Agent": "HTTPie/3.2.1",
     }
+    assert req.body is None
+
+    message = """GET /some-path/somewhere HTTP/1.1
+    Accept: */*
+    Accept-Encoding: gzip, deflate
+    Connection: stay-alive
+    Host: github.com
+    User-Agent: HTTPie/3.2.1
+
+        <title>god save the queen</title>
+
+        <body>body</body>
+    """
+    req = l_12.HttpRequest(message)
+    assert req.method == "GET"
+    assert req.path == "/some-path/somewhere"
+    assert req.http_version == "HTTP/1.1"
+    assert req.headers == {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate",
+        "Connection": "stay-alive",
+        "Host": "github.com",
+        "User-Agent": "HTTPie/3.2.1",
+    }
+    assert (
+        req.body
+        == "<title>god save the queen</title>\n\n        <body>body</body>"
+    )
+
+
+def test_03() -> None:
+    message = """HTTP/1.1 404 NOT FOUND
+    Content-Length: 48
+    Content-Type: application/json
+    Server: gunicorn/19.9.0
+
+    {"status_code": 404, "description": "no access"}
+    """
+
+    resp = l_12.HttpResponse(message)
+
+    assert resp.status_code == 404
+    assert resp.reason == "NOT FOUND"
+    assert resp.http_version == "HTTP/1.1"
+    assert resp.headers == {
+        "Content-Length": 48,
+        "Content-Type": "application/json",
+        "Server": "gunicorn/19.9.0",
+    }
+    assert resp.body == '{"status_code": 404, "description": "no access"}'
+    assert resp.is_valid()
+    assert resp.json() == {"status_code": 404, "description": "no access"}
+
+    message = """HTTP/1.1 404 NOT FOUND
+    Content-Length: 49
+    Content-Type: text/html
+    Server: gunicorn/19.9.0
+
+    {"status_code": 404, "description": "no access"}
+    """
+
+    resp = l_12.HttpResponse(message)
+    assert resp.body == '{"status_code": 404, "description": "no access"}'
+    assert not resp.is_valid()
+    assert resp.json() is None
+
+    message = """HTTP/1.1 404 NOT FOUND
+    Content-Length: 48
+    Content-Type: text/html
+    Server: gunicorn/19.9.0
+
+    {"status_code": 404, "description": "no access"}
+    """
+
+    resp = l_12.HttpResponse(message)
+
+    assert resp.body == '{"status_code": 404, "description": "no access"}'
+    assert resp.is_valid()
+    assert resp.json() is None
