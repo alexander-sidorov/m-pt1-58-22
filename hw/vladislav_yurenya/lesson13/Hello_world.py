f14d335097dfac07705f061e9941b8ab0f8f9d01
Vladislav Yurenya <228322fsa@mail.ru>
2022-12-30 23:46:25 +0300

    Vladislav Yurenya Lesson13

--- hw/vladislav_yurenya/lesson13/Hello_world.py
+++ hw/vladislav_yurenya/lesson13/Hello_world.py
@@ -1,1 +1,5 @@
-print("Hello world")
+from django.http import HttpRequest, HttpResponse
+
+
+def handle_vladislav_yurenya(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Hello, world!")
