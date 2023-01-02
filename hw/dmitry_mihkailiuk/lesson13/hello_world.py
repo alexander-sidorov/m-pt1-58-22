a9849f8510f1f0dc8df8d43f45f462a219e31c11
Mikhailiuk-Dmitriy <dmitriymikhailiuk@gmail.com>
2023-01-03 00:02:07 +0300

    lesson 13-3

--- hw/dmitry_mihkailiuk/lesson13/hello_world.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpRequest, HttpResponse
-
-
-def handle_dmitry_mikhailiuk(request: HttpRequest):
-    return HttpResponse("Hello World")
--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from hw.dmitry_mihkailiuk.lesson13.hello_world import handle_dmitry_mikhailiuk
+
+from hw.dmitry_mihkailiuk.lesson13.views import handle_dmitry_mikhailiuk
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("dmitry-mikhailiuk/", handle_dmitry_mikhailiuk),
-
+    path("~/dmitry_mihkailiuk/", handle_dmitry_mikhailiuk),
 ]
