b1be4946e8d7592c9fb8aeced61c942259ed063c
karmaxa <mikita.karmanaw@gmail.com>
2023-01-02 22:32:31 +0300

    help2

--- hw/mikita_karmanaw/lesson13/views.py
+++ hw/mikita_karmanaw/lesson13/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpResponse
+from django.http import HttpRequest
 
 
 def handle_mikita_karmanaw(request: HttpRequest) -> HttpResponse:
