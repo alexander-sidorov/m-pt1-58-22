fb6232c20a28e598646ce0df06969c3080fa0fdc
Alexander Sidorov <a375296648709@gmail.com>
2023-01-02 22:02:26 +0300

    fixed django view

--- hw/alexander_sidorov/lesson13/views.py
+++ hw/alexander_sidorov/lesson13/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpRequest, HttpResponse
+# pragma: no cover
 
 
-def handle_alexander_sidorov(request: HttpRequest):
-    return HttpResponse("Hello world")
+from django.http import HttpRequest
+from django.http import HttpResponse
+
+
+def handle_alexander_sidorov(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Hello from Alexander Sidorov")
