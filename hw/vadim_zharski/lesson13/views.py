670c7b533e7b2f0840fd8edf203da5605679428a
VadimZharsky <vadimzharsky1990@gmail.com>
2023-01-03 12:56:06 +0300

    Vadim Zharski lesson 13

--- hw/vadim_zharski/lesson13/views.py
+++ hw/vadim_zharski/lesson13/views.py
@@ -1,1 +1,6 @@
-from django.http import
+from django.http import HttpRequest
+from django.http import HttpResponse
+
+
+def handle_vadim_zharski(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Hello from Vadim Zharkski")
--- project/urls.py
+++ project/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 
 from hw.alexander_sidorov.lesson13.views import handle_alexander_sidorov
+from hw.vadim_zharski.lesson13.views import handle_vadim_zharski
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("~/alexander_sidorov/", handle_alexander_sidorov),
+    path("~/vadim_zharski", handle_vadim_zharski),
 ]
