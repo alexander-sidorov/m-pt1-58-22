e1e8713304d6e59103460e1726f3bd2d1e500a4b
EvgVavilov <785vvv@gmail.com>
2023-01-03 20:47:40 +0300

    v2

--- project/urls.py
+++ project/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from hw.eugene_vavilov.lesson13.lesson import handle_eugene_vavilov
 
 from hw.alexander_sidorov.lesson13.views import handle_alexander_sidorov
+from hw.eugene_vavilov.lesson13.views import handle_eugene_vavilov
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("~/alexander_sidorov/", handle_alexander_sidorov),
-    path("~/handle_eugene_vavilov/", handle_eugene_vavilov)
+    path("~/eugene_vavilov/", handle_eugene_vavilov),
 ]
