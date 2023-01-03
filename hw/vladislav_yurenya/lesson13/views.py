a79596fb5bd62845a189aa1a3095c8f6474086d2
Vladislav Yurenya <228322fsa@mail.ru>
2023-01-03 13:14:59 +0300

    fixed

--- hw/vladislav_yurenya/lesson13/views.py
+++ hw/vladislav_yurenya/lesson13/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest
+from django.http import HttpResponse
 
 
 def handle_vladislav_yurenya(request: HttpRequest) -> HttpResponse:
