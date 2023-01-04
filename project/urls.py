f84824634237c92461089a48c7b93f07bdb702de
AliakseiTsiukhai <118986917+AliakseiTsiukhai@users.noreply.github.com>
2023-01-04 16:20:50 +0300

    Alexey Tuyhai Lesson 13 (#130)

--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 
 from hw.alexander_sidorov.lesson13.views import handle_alexander_sidorov
+from hw.alexey_tyuhai.lesson13.hello_world import handle_alexey_tuyhai
 from hw.dmitry_mihkailiuk.lesson13.views import handle_dmitry_mikhailiuk
 from hw.eugene_lubimov.lesson13.views import my_view
 from hw.jana_sergienko.lesson13.views import handle_jana_sergienko
@@ -13,6 +14,7 @@ from hw.vladislav_yurenya.lesson13.views import handle_vladislav_yurenya
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("~/alexander_sidorov/", handle_alexander_sidorov),
+    path("~/alexey-tuyhai/", handle_alexey_tuyhai),
     path("~/dmitry_mihkailiuk/", handle_dmitry_mikhailiuk),
     path("~/eugene_lubimov/", my_view),
     path("~/jana_sergienko/", handle_jana_sergienko),
