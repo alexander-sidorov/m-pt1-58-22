05450736e063f473b1b387a3ed142508e1a7f2ca
mikitakarman <mikita.karmanaw@gmail.com>
2023-01-04 21:51:11 +0300

    money_html_page

--- app_mikita_karmanaw/views.py
+++ app_mikita_karmanaw/views.py
@@ -1,40 +1,31 @@
 from django.http import HttpRequest
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def hello_mk(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello from mikitakarman's app!")
 
 
-html = """
-<html>
-<head></head>
-<body>
-<form action="/~/mikita_karmanaw/lesson04/task01/" method="get">
-
-Rubles: <input type="text" name="r">
-Coins: <input type="text" name="c">
-Amount: <input type="text" name="a">
-
-<button type="submit">Calculate</button>
-
-<h3>{result}</h3>
-
-</form>
-</body>
-</html>
-"""
-
-
 def money(request: HttpRequest) -> HttpResponse:
     from hw.mikita_karmanaw.lesson04.lecture import task_01_money
 
     res = ""
+    rub = coins = amo = ""
 
     if request.GET:
         rub = int(request.GET["r"])
         coins = int(request.GET["c"])
         amo = int(request.GET["a"])
         res = task_01_money(rub, coins, amo)
-    html_out = html.format(result=res)
+    html_out = render(
+        request,
+        "app_mikita_karmanaw/index_task_01.html",
+        {
+            "result": res,
+            "r": rub,
+            "c": coins,
+            "a": amo,
+        },
+    )
     return HttpResponse(html_out)
