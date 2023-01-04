5f4eaf5ca617c56c51f46ba8643d7a32022b3a1d
Alexander Sidorov <a375296648709@gmail.com>
2023-01-04 21:16:21 +0300

    added html form

--- app_alexander_sidorov/views.py
+++ app_alexander_sidorov/views.py
@@ -8,9 +8,37 @@ def helloworld(request: HttpRequest) -> HttpResponse:
     return HttpResponse("hello from app 12ewadsadsada")
 
 
+HTML = """
+<html>
+<head>
+</head>
+<body>
+
+<form action="/~/alexander_sidorov/lesson04/task01/" method="get">
+
+Rubles: <input type="text" name="r">
+Coins: <input type="text" name="c">
+Amount: <input type="text" name="a">
+
+<button type="submit">Calculate</button>
+</form>
+
+<h1>{result}</h1>
+
+</body>
+</html>
+"""
+
+
 def handle_task_01_money(request: HttpRequest) -> HttpResponse:
-    rubles = int(request.GET["r"])
-    coins = int(request.GET["c"])
-    amount = int(request.GET["a"])
-    res = task_01_money(rubles, coins, amount)
-    return HttpResponse(res, content_type="text/plain")
+    result = ""
+
+    if request.GET:
+        rubles = int(request.GET["r"])
+        coins = int(request.GET["c"])
+        amount = int(request.GET["a"])
+        result = task_01_money(rubles, coins, amount)
+
+    html = HTML.format(result=result)
+
+    return HttpResponse(html)
