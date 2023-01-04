327ae9e4f2455ac39f4c3b20d982f585a6a5d160
Mikhailiuk-Dmitriy <61941364+Mikhailiuk-Dmitriy@users.noreply.github.com>
2023-01-04 15:35:17 +0300

    Dmitry Mikhailiuk Lesson 13 (#129)

--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 
 from hw.alexander_sidorov.lesson13.views import handle_alexander_sidorov
+from hw.dmitry_mihkailiuk.lesson13.views import handle_dmitry_mikhailiuk
 from hw.eugene_lubimov.lesson13.views import my_view
 from hw.jana_sergienko.lesson13.views import handle_jana_sergienko
 from hw.vladislav_yurenya.lesson13.views import handle_vladislav_yurenya
@@ -9,6 +10,7 @@ from hw.vladislav_yurenya.lesson13.views import handle_vladislav_yurenya
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("~/alexander_sidorov/", handle_alexander_sidorov),
+    path("~/dmitry_mihkailiuk/", handle_dmitry_mikhailiuk),
     path("~/eugene_lubimov/", my_view),
     path("~/jana_sergienko/", handle_jana_sergienko),
     path("~/vladislav_yurenya/", handle_vladislav_yurenya),
