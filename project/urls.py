fc26204a31597a1719365fd7ce49d40d97202cd2
Jana Sergienko <jana_monkey@mail.ru>
2023-01-04 15:08:24 +0300

    path jana_sergienko

--- project/urls.py
+++ project/urls.py
@@ -7,9 +7,5 @@ from hw.jana_sergienko.lesson13.views import handle_jana_sergienko
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("~/alexander_sidorov/", handle_alexander_sidorov),
-]
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
     path("~/jana_sergienko/", handle_jana_sergienko),
 ]
