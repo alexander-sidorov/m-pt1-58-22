5a5616ff2ef64de80121429898f7e77cef05b0c8
mikitakarman <mikita.karmanaw@gmail.com>
2023-01-04 20:24:08 +0300

    money

--- app_mikita_karmanaw/urls.py
+++ app_mikita_karmanaw/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from app_mikita_karmanaw.views import hello_mk
+from app_mikita_karmanaw.views import hello_mk, money
 
 urlpatterns = [
     path("", hello_mk),
+    path("money", money),
 ]
--- app_mikita_karmanaw/views.py
+++ app_mikita_karmanaw/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 
 def hello_mk(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello from mikitakarman's app!")
+
+
+def money(request: HttpRequest) -> HttpResponse:
+    from hw.mikita_karmanaw.lesson04.lecture import task_01_money
+    rub = request.GET["r"]
+    coins = request.GET["c"]
+    amo = request.GET["a"]
+    res = task_01_money(rub, coins, amo)
+    return HttpResponse(res)
