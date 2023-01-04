7d6af8776a702f66c50694352fec1d5c1f84d3d3
Jana Sergienko <jana_monkey@mail.ru>
2023-01-04 21:47:45 +0300

    html file

--- app_jana_sergienko/views.py
+++ app_jana_sergienko/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from hw.jana_sergienko.lesson04.lecture import task_01_money
 
@@ -8,25 +9,9 @@ def helloworld(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello from app")
 
 
-HTML = """
-<html>
-<head>
-</head>
-<body>
-<form action="/~/jana_sergienko/lesson04/task01/" method="get">
-Rubles: <input type="text" name="r">
-Coins: <input type="text" name="c">
-Amount: <input type="text" name="a">
-<button type="submit">Calculate</button>
-</form>
-<h1>{result}</h1>
-</body>
-</html>
-"""
-
-
 def handle_task_01_money(request: HttpRequest) -> HttpResponse:
     result = ""
+    rub = coin = amt = ""
 
     if request.GET:
         rub = int(request.GET["r"])
@@ -34,6 +19,13 @@ def handle_task_01_money(request: HttpRequest) -> HttpResponse:
         amt = int(request.GET["a"])
         result = task_01_money(rub, coin, amt)
 
-    html = HTML.format(result=result)
-
-    return HttpResponse(html)
+    return render(
+        request,
+        "app_jana_sergienko/task01.html",
+        {
+            "r": rub,
+            "c": coin,
+            "a": amt,
+            "result": result,
+        },
+    )
