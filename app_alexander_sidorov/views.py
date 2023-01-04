1687ea340f0ef1b327872897f3d5af3676f132f9
Alexander Sidorov <a375296648709@gmail.com>
2023-01-04 21:42:51 +0300

    added template

--- app_alexander_sidorov/views.py
+++ app_alexander_sidorov/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from hw.alexander_sidorov.lesson04.lecture import task_01_money
 
@@ -8,30 +9,9 @@ def helloworld(request: HttpRequest) -> HttpResponse:
     return HttpResponse("hello from app 12ewadsadsada")
 
 
-HTML = """
-<html>
-<head>
-</head>
-<body>
-
-<form action="/~/alexander_sidorov/lesson04/task01/" method="get">
-
-Rubles: <input type="text" name="r">
-Coins: <input type="text" name="c">
-Amount: <input type="text" name="a">
-
-<button type="submit">Calculate</button>
-</form>
-
-<h1>{result}</h1>
-
-</body>
-</html>
-"""
-
-
 def handle_task_01_money(request: HttpRequest) -> HttpResponse:
     result = ""
+    rubles = coins = amount = ""
 
     if request.GET:
         rubles = int(request.GET["r"])
@@ -39,6 +19,13 @@ def handle_task_01_money(request: HttpRequest) -> HttpResponse:
         amount = int(request.GET["a"])
         result = task_01_money(rubles, coins, amount)
 
-    html = HTML.format(result=result)
-
-    return HttpResponse(html)
+    return render(
+        request,
+        "app_alexander_sidorov/task01.html",
+        {
+            "r": rubles,
+            "c": coins,
+            "a": amount,
+            "result": result,
+        },
+    )
