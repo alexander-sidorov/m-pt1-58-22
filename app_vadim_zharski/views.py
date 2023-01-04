784c9e0de967b3e2ef905850fa28af3740a06615
vadim_zharski <vadimzharsky1990@gmail.com>
2023-01-04 20:03:25 +0300

    vadim-zharski-lesson14

--- app_vadim_zharski/views.py
+++ app_vadim_zharski/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest
+from django.http import HttpResponse
 
 
 def hello_world_vadim_zharski(request: HttpRequest) -> HttpResponse:
