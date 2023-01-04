3672637653ab25ab743a6d539c69d6b2cd0c7961
SergeiSakovich <119007148+SergeiSakovich@users.noreply.github.com>
2023-01-04 16:13:36 +0300

    Sergei Sakovich Lesson 13 (#141)

--- project/urls.py
+++ project/urls.py
@@ -6,6 +6,7 @@ from hw.dmitry_mihkailiuk.lesson13.views import handle_dmitry_mikhailiuk
 from hw.eugene_lubimov.lesson13.views import my_view
 from hw.jana_sergienko.lesson13.views import handle_jana_sergienko
 from hw.mikita_karmanaw.lesson13.views import handle_mikita_karmanaw
+from hw.sergey_sakovich.lesson13.views import handle_sergey_sakovich
 from hw.vadim_zharski.lesson13.views import handle_vadim_zharski
 from hw.vladislav_yurenya.lesson13.views import handle_vladislav_yurenya
 
@@ -16,6 +17,7 @@ urlpatterns = [
     path("~/eugene_lubimov/", my_view),
     path("~/jana_sergienko/", handle_jana_sergienko),
     path("~/mikita_karmanaw/", handle_mikita_karmanaw),
+    path("~/sergey_sakovich/", handle_sergey_sakovich),
     path("~/vadim_zharski", handle_vadim_zharski),
     path("~/vladislav_yurenya/", handle_vladislav_yurenya),
 ]
