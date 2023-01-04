23f214a1fac86b9980699658e87d71409eb3c9fc
mikitakarman <mikita.karmanaw@gmail.com>
2023-01-04 19:54:38 +0300

    fixes

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.urls import path
 from django.urls import include
+from django.urls import path
 
 from hw.alexander_sidorov.lesson13.views import handle_alexander_sidorov
 from hw.alexey_tyuhai.lesson13.hello_world import handle_alexey_tuyhai
@@ -8,7 +8,6 @@ from hw.dmitry_mihkailiuk.lesson13.views import handle_dmitry_mikhailiuk
 from hw.eugene_lubimov.lesson13.views import my_view
 from hw.eugene_vavilov.lesson13.views import handle_eugene_vavilov
 from hw.jana_sergienko.lesson13.views import handle_jana_sergienko
-from hw.mikita_karmanaw.lesson13.views import handle_mikita_karmanaw
 from hw.sergey_sakovich.lesson13.views import handle_sergey_sakovich
 from hw.vadim_zharski.lesson13.views import handle_vadim_zharski
 from hw.vladislav_yurenya.lesson13.views import handle_vladislav_yurenya
