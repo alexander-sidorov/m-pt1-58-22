a45d7c4c770d2a9ff002b02c87eb811f5f94a045
Alexander Sidorov <a375296648709@gmail.com>
2023-01-04 16:18:47 +0300

    formatted

--- hw/alexey_tyuhai/lesson13/hello_world.py
+++ hw/alexey_tyuhai/lesson13/hello_world.py
@@ -1,5 +1,6 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest
+from django.http import HttpResponse
 
 
-def handle_alexey_tuyhai(request: HttpRequest):
+def handle_alexey_tuyhai(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello world")
