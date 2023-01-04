8eac3cc2cc60dc517445125163a53a5cca97ab8f
varfalomey777 <119004516+varfalomey777@users.noreply.github.com>
2023-01-04 15:29:04 +0300

    Vladislav Yurenya lesson 13 (#137)

--- project/urls.py
+++ project/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 
 from hw.alexander_sidorov.lesson13.views import handle_alexander_sidorov
 from hw.jana_sergienko.lesson13.views import handle_jana_sergienko
+from hw.vladislav_yurenya.lesson13.views import handle_vladislav_yurenya
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("~/alexander_sidorov/", handle_alexander_sidorov),
     path("~/jana_sergienko/", handle_jana_sergienko),
+    path("~/vladislav_yurenya/", handle_vladislav_yurenya),
 ]
