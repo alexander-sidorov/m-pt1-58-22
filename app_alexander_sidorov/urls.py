cca4d70a8b697d242babba0b6c58d5ecba4db090
Alexander Sidorov <a375296648709@gmail.com>
2023-01-04 20:22:18 +0300

    added lesson04/task01

--- app_alexander_sidorov/urls.py
+++ app_alexander_sidorov/urls.py
@@ -4,4 +4,5 @@ from app_alexander_sidorov import views
 
 urlpatterns = [
     path("", views.helloworld),
+    path("lesson04/task01", views.handle_task_01_money),
 ]
--- app_alexander_sidorov/views.py
+++ app_alexander_sidorov/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpRequest
 from django.http import HttpResponse
 
+from hw.alexander_sidorov.lesson04.lecture import task_01_money
+
 
 def helloworld(request: HttpRequest) -> HttpResponse:
     return HttpResponse("hello from app 12ewadsadsada")
+
+
+def handle_task_01_money(request: HttpRequest) -> HttpResponse:
+    rubles = int(request.GET["r"])
+    coins = int(request.GET["c"])
+    amount = int(request.GET["a"])
+    res = task_01_money(rubles, coins, amount)
+    return HttpResponse(res, content_type="text/plain")
