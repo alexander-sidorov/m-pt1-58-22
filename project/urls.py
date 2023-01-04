7af9e8ecb36768531348fa773421a56e1ed35bfe
SergeiSakovich <stem692@gmail.com>
2023-01-04 16:00:17 +0300

    13

--- project/urls.py
+++ project/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 
 from hw.alexander_sidorov.lesson13.views import handle_alexander_sidorov
+from hw.sergey_sakovich.lesson13.views import handle_sergey_sakovich
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("~/alexander_sidorov/", handle_alexander_sidorov),
+    path("~/sergey_sakovich/", handle_sergey_sakovich),
 ]
