55627063bf30f68eaac7cc9e989d0d3dd0af7a0e
Jana Sergienko <jana_monkey@mail.ru>
2023-01-04 14:58:59 +0300

    url

--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from hw.alexander_sidorov.lesson13.views import handle_alexander_sidorov
+from hw.jana_sergienko.lesson13.views import handle_jana_sergienko
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("~/alexander_sidorov/", handle_alexander_sidorov),
+    path("~/jana_sergienko/", handle_jana_sergienko),
 ]
