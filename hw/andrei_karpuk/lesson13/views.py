ef682963d125c3d11994e2f3baee7e36b100cdf5
andrei_karpuk <andreigalardo@gmail.com>
2023-01-04 19:08:27 +0300

    lesson 13 upd from itacademy PC

--- hw/andrei_karpuk/lesson13/views.py
+++ hw/andrei_karpuk/lesson13/views.py
@@ -2,5 +2,5 @@ from django.http import HttpRequest
 from django.http import HttpResponse
 
 
-def my_view(request: HttpRequest) -> HttpResponse:
+def ak_view(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello from Andrei Karpuk")
