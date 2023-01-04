12eb3bd382934bf5683ecab086d91d33940a2d60
Jana Sergienko <jana_monkey@mail.ru>
2023-01-04 21:26:44 +0300

    HTML

--- app_jana_sergienko/views.py
+++ app_jana_sergienko/views.py
@@ -8,9 +8,32 @@ def helloworld(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello from app")
 
 
+HTML = """
+<html>
+<head>
+</head>
+<body>
+<form action="/~/jana_sergienko/lesson04/task01/" method="get">
+Rubles: <input type="text" name="r">
+Coins: <input type="text" name="c">
+Amount: <input type="text" name="a">
+<button type="submit">Calculate</button>
+</form>
+<h1>{result}</h1>
+</body>
+</html>
+"""
+
+
 def handle_task_01_money(request: HttpRequest) -> HttpResponse:
-    rub = int(request.GET["r"])
-    coin = int(request.GET["c"])
-    amt = int(request.GET["a"])
-    res = task_01_money(rub, coin, amt)
-    return HttpResponse(res, content_type="text/plain")
+    result = ""
+
+    if request.GET:
+        rub = int(request.GET["r"])
+        coin = int(request.GET["c"])
+        amt = int(request.GET["a"])
+        result = task_01_money(rub, coin, amt)
+
+    html = HTML.format(result=result)
+
+    return HttpResponse(html)
