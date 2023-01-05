ea0fbab0ae34590b16a5c0620806daa9d2e06920
Vladislav Yurenya <228322fsa@mail.ru>
2023-01-05 10:54:25 +0300

    .

--- app_vladislav_yurenya/urls.py
+++ app_vladislav_yurenya/urls.py
@@ -4,4 +4,5 @@ from app_vladislav_yurenya import views
 
 urlpatterns = [
     path("", views.helloworld),
+    path("lesson04/task01/", views.my_money),
 ]
--- app_vladislav_yurenya/views.py
+++ app_vladislav_yurenya/views.py
@@ -1,5 +1,30 @@
 from django.http import HttpResponse,HttpRequest
+from django.shortcuts import render
+
+from hw.vladislav_yurenya.lesson_04.lecture import task_01_money
 
 
 def helloworld(request: HttpRequest) -> HttpResponse:
     return HttpResponse('HELLO FROM APP!')
+
+
+def my_money(request: HttpRequest) -> HttpResponse:
+    result = ""
+    rubles = coins = amount = ""
+
+    if request.GET:
+        rubles=int(request.GET['r'])
+        coins = int(request.GET['c'])
+        amount = int(request.GET['a'])
+        result=task_01_money(rubles,coins,amount)
+
+    return render(
+        request,
+        "app_vladislav_yurenya/task01.html",
+        {
+            "r": rubles,
+            "c": coins,
+            "a": amount,
+            "result": result,
+        },
+    )
