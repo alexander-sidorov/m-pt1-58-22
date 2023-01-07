616507d8445e35c7903b470ee8d4808ad49f1712
Maksim_Baranau <emailhere@mail.ru>
2023-01-07 22:48:28 +0300

    task1and2

---
+++ hw/maksim_baranau/lesson12/lesson.py
@@ -0,0 +1,75 @@
+class Url:
+    def __init__(self, url: str) -> None:  # noqa: CCR001
+        self.scheme = None
+        self.username = None
+        self.password = None
+        self.host = None
+        self.port = None
+        self.path = None
+        self.query = None
+        self.fragment = None
+
+        self.scheme = url[: url.find("://")]
+        url = url[url.find("://") + 3 :]
+        if "#" in url:
+            self.fragment = url[url.find("#") + 1 :]
+            url = url[: url.find("#")]
+        if "@" in url:
+            if ":" in url:
+                self.username = url[: url.find(":")]
+                url = url[url.find(":") + 1 :]
+                self.password = url[: url.find("@")]
+                url = url[url.find("@") + 1 :]
+            else:
+                self.username = url[: url.find("@")]
+                url = url[url.find("@") + 1 :]
+                if "/" in url:
+                    self.host = url[: url.find("/")]
+                    url = url[url.find("/") + 1 :]
+                if "?" in url:
+                    urlhost1 = url[: url.find("?")]
+                    if urlhost1 == "":
+                        ...
+                    else:
+                        self.host = urlhost1
+                        url = url[url.find("?") :]
+                else:
+                    self.host = url
+        else:
+            if "/" in url or "?" in url:
+                self.host = url[: url.find("/")]
+                url = url.replace(self.host, "")
+            else:
+                self.host = url
+        if "?" in url:
+            self.query = url[url.find("?") + 1 :]
+            url = url[: url.find("?")]
+        if ":" in url:
+            self.host = url[: url.find(":")]
+            url = url[url.find(":") + 1 :]
+        if "/" in url:
+            self.path = url[url.find("/") :]
+            url = url[: url.find("/")]
+        if url.isnumeric():
+            self.port = url
+
+
+class HttpRequest:
+    def __init__(self, req: str) -> None:
+        self.method: None | str = None
+        self.path: None | str = None
+        self.http_version: None | str = None
+        self.headers: dict[str, str] = {}
+        self.body: None | str = None
+        body_cut_pos = req.find("\n\n") + 2
+        self.body = req[body_cut_pos:]
+        if self.body == "":
+            self.body = None
+        req = req[: req.find("\n\n")]
+        lines = req.splitlines()
+        head = lines[0].split(" ")
+        self.method, self.path, self.http_version = head
+        del lines[0]
+        for line in lines:
+            header = line.split(": ")
+            self.headers[header[0]] = header[1]
---
+++ hw/maksim_baranau/lesson12/lesson_test.py
@@ -0,0 +1,76 @@
+from hw.maksim_baranau.lesson12.lesson import Url
+from hw.maksim_baranau.lesson12.lesson import HttpRequest
+
+
+def test_task_1() -> None:
+    url = Url("http://google.com")
+    assert url.scheme == "http"
+    assert url.username is None
+    assert url.password is None
+    assert url.host == "google.com"
+    assert url.port is None
+    assert url.path is None
+    assert url.query is None
+    assert url.fragment is None
+
+    url = Url("postgresql://u:p@db:5432/dbname?opt=1&xyz=2#f")
+    assert url.scheme == "postgresql"
+    assert url.username == "u"
+    assert url.password == "p"  # noqa S105
+    assert url.host == "db"
+    assert url.port == "5432"
+    assert url.path == "/dbname"
+    assert url.query == "opt=1&xyz=2"
+    assert url.fragment == "f"
+
+    url = Url("vnc://user@host?query=q")
+    assert url.scheme == "vnc"
+    assert url.username == "user"
+    assert url.password is None
+    assert url.host == "host"
+    assert url.port is None
+    assert url.path is None
+    assert url.query == "query=q"
+    assert url.fragment is None
+
+    url = Url("vnc://user@host/?query=q")
+    assert url.scheme == "vnc"
+    assert url.username == "user"
+    assert url.password is None
+    assert url.host == "host"
+    assert url.port is None
+    assert url.path is None
+    assert url.query == "query=q"
+    assert url.fragment is None
+
+    url = Url("ftp://a.b.c.host/p/a/t/h?query=q&f=f#f")
+    assert url.scheme == "ftp"
+    assert url.username is None
+    assert url.password is None
+    assert url.host == "a.b.c.host"
+    assert url.port is None
+    assert url.path == "/p/a/t/h"
+    assert url.query == "query=q&f=f"
+    assert url.fragment == "f"
+
+
+def test_task_2() -> None:
+    message = """HEAD / HTTP/1.1
+Accept: */*
+Accept-Encoding: gzip, deflate
+Connection: keep-alive
+Host: github.com
+User-Agent: HTTPie/3.2.1
+"""
+    req = HttpRequest(message)
+    assert req.method == "HEAD"
+    assert req.path == "/"
+    assert req.http_version == "HTTP/1.1"
+    assert req.headers == {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate",
+        "Connection": "keep-alive",
+        "Host": "github.com",
+        "User-Agent": "HTTPie/3.2.1",
+    }
+    assert req.body is None
