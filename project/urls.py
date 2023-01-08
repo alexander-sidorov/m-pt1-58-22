7f216a9875219cdb1e056690075455e205086c02
AliakseiTsiukhai <leha26061992@gmail.com>
2023-01-08 14:19:55 +0300

    fixed hw 13

--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
-from hw.alexey_tyuhai.lesson13.hello_world import handle_alexey_tuyhai
+from hw.alexey_tyuhai.lesson13.views import handle_alexey_tuyhai
 from hw.dmitry_mihkailiuk.lesson13.views import handle_dmitry_mikhailiuk
 from hw.eugene_lubimov.lesson13.views import my_view
 from hw.eugene_vavilov.lesson13.views import handle_eugene_vavilov
