26231fdf30da1bb76c585c9e882f324b39b6ae7e
Alexander Sidorov <76160017+alexander-sidorov@users.noreply.github.com>
2023-01-09 20:26:40 +0300

    fixed code

--- app_maksim_baranau/views.py
+++ app_maksim_baranau/views.py
@@ -11,14 +11,14 @@ def helloworld(request: HttpRequest) -> HttpResponse:
     return HttpResponse("hello from APP")
 
 
-def handle_task_01_money(request: HttpRequest) -> Decimal:
-    dec_rubles = dec_coins = dec_amount = ""
-    result = ""
+def handle_task_01_money(request: HttpRequest) -> HttpResponse:
+    dec_rubles = dec_coins = dec_amount = 0
+    result = Decimal(0)
     if request.GET:
-        dec_rubles = Decimal(request.GET["r"])
-        dec_coins = Decimal(request.GET["c"])
-        dec_amount = Decimal(request.GET["a"])
-        result = (dec_rubles + (dec_coins / 100)) * dec_amount
+        dec_rubles = int(request.GET["r"])
+        dec_coins = int(request.GET["c"])
+        dec_amount = int(request.GET["a"])
+        result = task_01_money(dec_rubles, dec_coins, dec_amount)
 
     return render(
         request, "app_maksim_baranau/task01.html", {"result": result}
