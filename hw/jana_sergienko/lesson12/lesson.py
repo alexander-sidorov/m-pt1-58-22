d0a24a4683b979570624796566cd0ca3e36bebef
Jana Sergienko <jana_monkey@mail.ru>
2023-01-09 14:24:29 +0300

    lesson 12

--- hw/jana_sergienko/lesson12/lesson.py
+++ hw/jana_sergienko/lesson12/lesson.py
@@ -1,10 +1,22 @@
 class Url:
-    def get_url(url: str) -> dict:
+    def get_schema(url: str) -> dict:
         dict_url = {}
         new_url = url
-        schema = url.split("://")[0]
 
+        schema = url.split("://")[0]
         dict_url.update({"schema": schema})
+
+        user = url.split(":")[1].replace("//", "")
+        dict_url.update({"user:": user})
+
+        password = url.split(":")[2].split("@")[0]
+        dict_url.update({"password": password})
+
+        host = url.split(":")[2].split("/")[0]
+        dict_url.update({"host": host})
+
+        port = url.split(":")[3].split("/")[0]
+        dict_url
 
 
 
