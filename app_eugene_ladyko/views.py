f9c1513e3f87c59b67246071a086fda4492ac8e8
Ladyka-Yauhen <angelby13@gmail.com>
2023-01-11 21:22:16 +0300

    4

--- app_eugene_ladyko/views.py
+++ app_eugene_ladyko/views.py
@@ -7,12 +7,15 @@ from django.shortcuts import render
 
 from hw.eugene_ladyko.lesson04.lecture import task_01_money
 
+
 def helloworld(request: HttpRequest) -> HttpResponse:
     return HttpResponse("hello from app 12ewadsadsada")
+
 
 def handle_task_01_money(request: HttpRequest) -> HttpResponse:
     result = Decimal()
     rubles = coins = amount = 0
+
 
     if request.GET:
         rubles = int(request.GET["r"])
@@ -30,6 +33,14 @@ def handle_task_01_money(request: HttpRequest) -> HttpResponse:
             "result": result,
         },
     )
+    if not request.GET:
+        return render(request, "app_eugene_ladyko/task01.html")
+
+    rubles = int(request.GET["r"])
+    coins = int(request.GET["c"])
+    amount = int(request.GET["a"])
+    result = task_01_money(rubles, coins, amount)
+
     payload = {
         "data": float(result),
     }
