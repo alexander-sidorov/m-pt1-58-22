db3d4a0308b225108c43d9bc570f3762e1c64eff
mikitakarman <mikita.karmanaw@gmail.com>
2023-01-11 20:14:06 +0300

    json added

--- app_mikita_karmanaw/urls.py
+++ app_mikita_karmanaw/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 
 from app_mikita_karmanaw.views import hello_mk
 from app_mikita_karmanaw.views import money
+from app_mikita_karmanaw.views import palindrom
+from app_mikita_karmanaw.views import sign
+from app_mikita_karmanaw.views import triangle
 
 urlpatterns = [
     path("", hello_mk),
     path("lesson04/task01/", money),
+    path("lesson04/task02/", sign),
+    path("lesson04/task03/", triangle),
+    path("lesson04/task04/", palindrom),
 ]
--- app_mikita_karmanaw/views.py
+++ app_mikita_karmanaw/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import HttpRequest
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -15,19 +17,91 @@ def money(request: HttpRequest) -> HttpResponse:
     coins: str | int = ""
     amo: str | int = ""
 
+    if not request.GET:
+        html_out = render(
+            request,
+            "app_mikita_karmanaw/index_task_01.html",
+            {
+                "r": rub,
+                "c": coins,
+                "a": amo,
+            },
+        )
+        return HttpResponse(html_out)
     if request.GET:
         rub = int(request.GET["r"])
         coins = int(request.GET["c"])
         amo = int(request.GET["a"])
         res = str(task_01_money(rub, coins, amo))
-    html_out = render(
-        request,
-        "app_mikita_karmanaw/index_task_01.html",
-        {
+    payload = {
+        "data": {
             "result": res,
             "r": rub,
             "c": coins,
             "a": amo,
         },
-    )
-    return HttpResponse(html_out)
+    }
+    return HttpResponse(json.dumps(payload), content_type="application/json")
+
+
+def sign(request: HttpRequest) -> HttpResponse:
+    from hw.mikita_karmanaw.lesson04.lecture import task_02_sign
+
+    if not request.GET:
+        return HttpResponse("no data")
+    else:
+        num = int(request.GET["number"])
+        res = str(task_02_sign(num))
+        payload = {
+            "data": {
+                "number": num,
+                "sign": res,
+            },
+        }
+        return HttpResponse(
+            json.dumps(payload), content_type="application/json"
+        )
+
+
+def triangle(request: HttpRequest) -> HttpResponse:
+    from hw.mikita_karmanaw.lesson04.lecture import task_03_triangle
+
+    if not request.GET:
+        return HttpResponse("no data")
+    else:
+        side1, side2, side3 = (
+            int(request.GET["side1"]),
+            int(request.GET["side2"]),
+            int(request.GET["side3"]),
+        )
+        res = task_03_triangle(side1, side2, side3)
+        payload = {
+            "data": {
+                "side1": side1,
+                "side2": side2,
+                "side3": side3,
+                "triangle": res,
+            },
+        }
+        return HttpResponse(
+            json.dumps(payload), content_type="application/json"
+        )
+
+
+def palindrom(request: HttpRequest) -> HttpResponse:
+    from hw.mikita_karmanaw.lesson04.lecture import task_04_palindrom
+
+    if not request.GET:
+        return HttpResponse("no data")
+    else:
+        string = request.GET["str"]
+        res = task_04_palindrom(string)
+        payload = {
+            "data": {
+                "string": string,
+                "palindrom": res,
+            },
+        }
+        return HttpResponse(
+            json.dumps(payload), content_type="application/json"
+        )
