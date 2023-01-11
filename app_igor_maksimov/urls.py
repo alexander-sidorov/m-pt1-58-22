71280b5d2a247f17441e340a9d647e8580482c31
User <you@example.com>
2023-01-11 21:55:16 +0300

    'lesson_15'

--- app_igor_maksimov/urls.py
+++ app_igor_maksimov/urls.py
@@ -5,4 +5,6 @@ from app_igor_maksimov import views
 urlpatterns = [
     path("", views.helloworld),
     path("lesson04/task01/", views.handle_task_01_money),
+    path("lesson04/task02/", views.handle_task_02_sign),
+    path("lesson04/task03/", views.handle_task_triagle)
 ]
--- app_igor_maksimov/views.py
+++ app_igor_maksimov/views.py
@@ -1,26 +1,53 @@
 from django.http import HttpRequest
 from django.http import HttpResponse
 from django.shortcuts import render
+import json
 
-from hw.alexander_sidorov.lesson04.lecture import task_01_money
+from hw.alexander_sidorov.lesson04.lecture import task_01_money, task_02_sign,task_03_triangle
 
 
 def helloworld(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("hello from app 12ewadsadsada")
-
+    if request:
+        return render(request, 'app_igor_maksimov/main.html')
+    else:
+        return HttpResponse('Hello world')
 
 def handle_task_01_money(request: HttpRequest) -> HttpResponse:
-    result = ""
-    rubles = coins = amount = ""
-    if request.GET:
-        rubles = int(request.GET["r"])
-        coins = int(request.GET["c"])
-        amount = int(request.GET["a"])
-        result = task_01_money(rubles, coins, amount)
+   
+    if not request.GET:
+        return render(request, 'app_igor_maksimov/task01.html')
 
-    return render(request,"app_igor_maksimov/task01.html",{
-            "r": rubles,
-            "c": coins,
-            "a": amount,
-            "result": result,
-        },)    
+    rubles = int(request.GET["r"])
+    coins = int(request.GET["c"])
+    amount = int(request.GET["a"])
+    result = task_01_money(rubles, coins, amount)
+
+    payload = {
+        'data': float(result) 
+    }
+    return HttpResponse(json.dumps(payload), content_type= "application/json")
+ 
+
+def handle_task_02_sign(request: HttpRequest)-> HttpResponse:
+   if not request.GET:
+       return render(request, 'app_igor_maksimov/task04_02.html')
+       
+   number = int(request.GET['x'])
+   result = task_02_sign(number)
+   payload = {
+       'data': result
+   }
+   return HttpResponse(json.dumps(payload), content_type= "application/json")
+
+def handle_task_triagle(request:HttpRequest)-> HttpResponse:
+    if not request.GET:
+        return render(request, 'app_igor_maksimov/task04_03.html')
+    side1= request.GET['a']
+    side2= request.GET['b']
+    side3= request.GET['c']
+    result = task_03_triangle(side1,side2,side3)
+
+    payload = {
+        'data': result
+    }
+    return HttpResponse(json.dumps(payload), content_type= "application/json")
