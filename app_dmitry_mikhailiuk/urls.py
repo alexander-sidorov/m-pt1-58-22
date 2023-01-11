11bfcefaa3f071965b12fa3838a435380b66cb1f
Mikhailiuk-Dmitriy <mds5@mail.ru>
2023-01-11 21:56:21 +0300

    lesson 15-2-3

--- app_dmitry_mikhailiuk/urls.py
+++ app_dmitry_mikhailiuk/urls.py
@@ -5,4 +5,8 @@ from app_dmitry_mikhailiuk import views
 urlpatterns = [
     path("", views.helloworld),
     path("lesson04/task01/", views.handle_task_01_money),
+    path("lesson04/task02/", views.handle_task_02_sign),
+    path("lesson04/task03/", views.handle_task_03_triangle),
+    path("lesson04/task04/", views.handle_task_04_palindrom),
+    path("lesson06/task01/", views.handle_task_01_boundary),
 ]
--- app_dmitry_mikhailiuk/views.py
+++ app_dmitry_mikhailiuk/views.py
@@ -5,10 +5,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from hw.dmitry_mihkailiuk.lesson04.lecture import task_01_money
+from hw.dmitry_mihkailiuk.lesson04.lecture import task_02_sign
+from hw.dmitry_mihkailiuk.lesson04.lecture import task_03_triangle
+from hw.dmitry_mihkailiuk.lesson04.lecture import task_04_palindrom
+from hw.dmitry_mihkailiuk.lesson06.tasks import task_01_boundary
 
 
 def helloworld(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello from app!")
+    return render(request, "app_dmitry_mikhailiuk/index.html")
 
 
 def handle_task_01_money(request: HttpRequest) -> HttpResponse:
@@ -25,3 +29,59 @@ def handle_task_01_money(request: HttpRequest) -> HttpResponse:
     }
 
     return HttpResponse(json.dumps(payload), content_type="application/json")
+
+
+def handle_task_02_sign(request: HttpRequest) -> HttpResponse:
+    if request.GET:
+        number = int(request.GET["n"])
+        result = task_02_sign(number)
+
+        payload = {
+            "data": result,
+        }
+
+        return HttpResponse(json.dumps(payload), content_type="application/json")
+
+    return render(request, "app_dmitry_mikhailiuk/task02.html")
+
+
+def handle_task_03_triangle(request: HttpRequest) -> HttpResponse:
+    if request.GET:
+        side1 = float(request.GET["s1"])
+        side2 = float(request.GET["s2"])
+        side3 = float(request.GET["s3"])
+        result = task_03_triangle(side1, side2, side3)
+
+        payload = {
+            "data": result,
+        }
+
+        return HttpResponse(json.dumps(payload), content_type="application/json")
+
+
+def handle_task_04_palindrom(request: HttpRequest) -> HttpResponse:
+    if request.GET:
+        st = request.GET["s"]
+        result = task_04_palindrom(st)
+
+        payload = {
+            "data": result,
+        }
+
+        return HttpResponse(json.dumps(payload), content_type="application/json")
+
+
+def handle_task_01_boundary(request: HttpRequest) -> HttpResponse:
+    if request.GET:
+        tup = tuple(request.GET["s"])
+        first, last = task_01_boundary(tup)
+
+        payload = {
+            "data": {
+                "original": request.GET["s"],
+                "first": first,
+                "last": last
+            }
+        }
+
+        return HttpResponse(json.dumps(payload), content_type="application/json")
