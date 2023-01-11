522980bc745404a1a8dc5be2527aa210f3324f0f
Alexander Sidorov <76160017+alexander-sidorov@users.noreply.github.com>
2023-01-11 19:41:16 +0300

    implemented json for task money

--- app_alexander_sidorov/views.py
+++ app_alexander_sidorov/views.py
@@ -1,8 +1,8 @@
+import json
 from decimal import Decimal
 
 from django.http import HttpRequest
 from django.http import HttpResponse
-from django.shortcuts import render
 
 from hw.alexander_sidorov.lesson04.lecture import task_01_money
 
@@ -13,7 +13,6 @@ def helloworld(request: HttpRequest) -> HttpResponse:
 
 def handle_task_01_money(request: HttpRequest) -> HttpResponse:
     result = Decimal()
-    rubles = coins = amount = 0
 
     if request.GET:
         rubles = int(request.GET["r"])
@@ -21,13 +20,8 @@ def handle_task_01_money(request: HttpRequest) -> HttpResponse:
         amount = int(request.GET["a"])
         result = task_01_money(rubles, coins, amount)
 
-    return render(
-        request,
-        "app_alexander_sidorov/task01.html",
-        {
-            "r": rubles,
-            "c": coins,
-            "a": amount,
-            "result": result,
-        },
-    )
+    payload = {
+        "data": float(result),
+    }
+
+    return HttpResponse(json.dumps(payload), content_type="application/json")
