02864adb3caf2e7bf3dded502b685c0cfa6dc71a
EvgVavilov <785vvv@gmail.com>
2023-01-11 19:57:59 +0300

    add task2

--- app_eugene_vavilov/views.py
+++ app_eugene_vavilov/views.py
@@ -1,3 +1,4 @@
+import json
 from decimal import Decimal
 
 from django.http import HttpRequest
@@ -5,6 +6,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from hw.eugene_vavilov.lesson04.lecture import task_01_money
+from hw.eugene_vavilov.lesson04.lecture import task_02_sign
 
 
 def helloworld(request: HttpRequest) -> HttpResponse:
@@ -18,21 +20,39 @@ def handle_task_01_money(request: HttpRequest) -> HttpResponse:
     result: str | Decimal
 
     result = ""
-    rubles = coins = amount = ""
+    rubles = coins = amount = 0
 
     if request.GET:
         rubles = int(request.GET["r"])
         coins = int(request.GET["c"])
         amount = int(request.GET["a"])
         result = task_01_money(rubles, coins, amount)
+    else:
+        return render(
+            request,
+            "app_alexander_sidorov/task01.html",
+            {
+                "r": rubles,
+                "c": coins,
+                "a": amount,
+                "result": result,
+            },
+        )
 
-    return render(
-        request,
-        "app_eugene_vavilov/lesson04.html",
-        {
-            "r": rubles,
-            "c": coins,
-            "a": amount,
-            "result": result,
-        },
-    )
+    payload = {
+        "data": int(result),
+    }
+
+    return HttpResponse(json.dumps(payload), content_type="application/json")
+
+
+def handle_task_02_sign(request: HttpRequest) -> HttpResponse:
+    if request.GET:
+        number = int(request.GET["n"])
+        result = task_02_sign(number)
+
+    payload = {
+        "data": float(result),
+    }
+
+    return HttpResponse(json.dumps(payload), content_type="application/json")
