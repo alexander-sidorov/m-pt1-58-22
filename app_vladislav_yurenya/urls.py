9771b2e9e60c5072ad6f052713b0bc9dc874f484
Vladislav Yurenya <228322fsa@mail.ru>
2023-01-11 20:54:37 +0300

    Vladislav Yurenya Lesson 15

--- app_vladislav_yurenya/urls.py
+++ app_vladislav_yurenya/urls.py
@@ -5,4 +5,7 @@ from app_vladislav_yurenya import views
 urlpatterns = [
     path("", views.helloworld),
     path("lesson04/task01/", views.my_money, name="money"),
+    path("lesson04/task02/", views.sign, name="sign"),
+    path("lesson04/task03/", views.triangle,name='triangle'),
+    path("lesson04/task04/", views.palindrom,name='palindrom'),
 ]
--- app_vladislav_yurenya/views.py
+++ app_vladislav_yurenya/views.py
@@ -1,10 +1,14 @@
+import json
 from decimal import Decimal
 
-from django.http import HttpRequest
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from requests import request
 
 from hw.vladislav_yurenya.lesson_04.lecture import task_01_money
+from hw.vladislav_yurenya.lesson_04.lecture import task_02_sign
+from hw.vladislav_yurenya.lesson_04.lecture import task_03_triangle
+from hw.vladislav_yurenya.lesson_04.lecture import task_04_palindrom
 
 
 def helloworld(request: HttpRequest) -> HttpResponse:
@@ -19,19 +23,48 @@ def my_money(request: HttpRequest) -> HttpResponse:
     coins: str | int = ""
     amount: str | int = ""
 
-    if request.GET:
-        rubles = int(request.GET["r"])
-        coins = int(request.GET["c"])
-        amount = int(request.GET["a"])
-        result = task_01_money(rubles, coins, amount)
+    if not request.GET:
+        return render(request, "app_vladislav_yurenya/task01.html")
+    rubles = int(request.GET["r"])
+    coins = int(request.GET["c"])
+    amount = int(request.GET["a"])
+    result = task_01_money(rubles, coins, amount)
+    payload = {"data": float(result)}
+    return HttpResponse(json.dumps(payload), content_type="application/json")
 
-    return render(
-        request,
-        "app_vladislav_yurenya/task01.html",
-        {
-            "r": rubles,
-            "c": coins,
-            "a": amount,
-            "result": result,
-        },
-    )
+
+def sign(request: HttpRequest) -> HttpResponse:
+    number: str | int = ""
+    res: int | str = ""
+    if not request.GET:
+        return render(request, "app_vladislav_yurenya/task02.html")
+    number = int(request.GET["number"])
+    res = task_02_sign(number)
+    payloads = {"data": res}
+    return HttpResponse(json.dumps(payloads), content_type="application/json")
+
+
+def triangle(request: HttpRequest) -> HttpResponse:
+    result = bool()
+    rubles: str | int = ""
+    coins: str | int = ""
+    amount: str | int = ""
+    if not request.GET:
+        return render(request, "app_vladislav_yurenya/task03.html")
+    side1 = float(request.GET["s1"])
+    side2 = float(request.GET["s2"])
+    side3 = float(request.GET["s3"])
+    result = task_03_triangle(side1, side2, side3)
+    payloads = {"data": bool(result)}
+    return HttpResponse(json.dumps(payloads), content_type="application/json")
+
+
+def palindrom(request: HttpRequest) -> HttpResponse:
+    string: str = ""
+    if not request.GET:
+        return render(request, "app_vladislav_yurenya/task04.html")
+
+    string = str(request.GET["string"])
+    result = task_04_palindrom(string)
+    payloads = {"data": bool(result)}
+    return HttpResponse(json.dumps(payloads), content_type="application/json")
