044a17deb0242aa04ef57106b03225d867038532
vadim_zharski <vadimzharsky1990@gmail.com>
2023-01-12 18:00:46 +0300

    Vadim Zharski lesson15

--- app_vadim_zharski/views.py
+++ app_vadim_zharski/views.py
@@ -22,7 +22,7 @@ def task_money(request: HttpRequest) -> HttpResponse:
         coins = int(request.GET["coins"])
         amount = int(request.GET["amount"])
         result = float(les4.task_01_money(rubles, coins, amount))
-        res: dict = {
+        content: dict = {
             "data": {
                 "rubles": rubles,
                 "coins": coins,
@@ -30,91 +30,75 @@ def task_money(request: HttpRequest) -> HttpResponse:
                 "result": result,
             }}
         if 'html' in request.GET:
-            return render(request, "app_vadim_zharski/task_01.html", res)
+            return render(request, "app_vadim_zharski/task_01.html", content)
         if 'json' in request.GET:
-            return HttpResponse(json.dumps(res), content_type="application/json")
+            return HttpResponse(json.dumps(content), content_type="application/json")
     return render(request, "app_vadim_zharski/task_01.html")
 
 
 def task_sign(request: HttpRequest) -> HttpResponse:
-    num: str | Any = ""
-    result: str | int = ""
-    content: dict = {
-        "num": num,
-        "data": result,
-    }
-    if not request.GET:
-        return render(request, "app_vadim_zharski/task_02.html", content)
-
-    num = float(request.GET["num"])
-    result = les4.task_02_sign(num)
-    payload: dict = {
-        "num": num,
-        "data": result,
-    }
-    return HttpResponse(json.dumps(payload), content_type="application/json")
+    if request.GET:
+        num = float(request.GET["num"])
+        result = les4.task_02_sign(num)
+        content: dict = {
+            "data": {
+                "num": num,
+                "result": result,
+            }}
+        if 'html' in request.GET:
+            return render(request, "app_vadim_zharski/task_02.html", content)
+        if 'json' in request.GET:
+            return HttpResponse(json.dumps(content), content_type="application/json")
+    return render(request, "app_vadim_zharski/task_02.html")
 
 
 def task_triangle(request: HttpRequest) -> HttpResponse:
-    side1: str | float = ""
-    side2: str | float = ""
-    side3: str | float = ""
-    result: str | bool = ""
-    content: dict = {
-        "side1": side1,
-        "side2": side2,
-        "side3": side3,
-        "data": result,
-    }
-    if not request.GET:
-        return render(request, "app_vadim_zharski/task_03.html", content)
-
-    side1 = float(request.GET["side1"])
-    side2 = float(request.GET["side2"])
-    side3 = float(request.GET["side3"])
-    result = les4.task_03_triangle(side1, side2, side3)
-    payload: dict = {
-        "side1": side1,
-        "side2": side2,
-        "side3": side3,
-        "data": result,
-    }
-    return HttpResponse(json.dumps(payload), content_type="application/json")
+    if request.GET:
+        side1 = float(request.GET["side1"])
+        side2 = float(request.GET["side2"])
+        side3 = float(request.GET["side3"])
+        result = les4.task_03_triangle(side1, side2, side3)
+        content: dict = {
+            "data": {
+                "side1": side1,
+                "side2": side2,
+                "side3": side3,
+                "result": result,
+            }}
+        if 'html' in request.GET:
+            return render(request, "app_vadim_zharski/task_03.html", content)
+        if 'json' in request.GET:
+            return HttpResponse(json.dumps(content), content_type="application/json")
+    return render(request, "app_vadim_zharski/task_03.html")
 
 
 def task_palindrom(request: HttpRequest) -> HttpResponse:
-    string: str = ""
-    result: str | bool = ""
-    content: dict = {
-        "string": string,
-        "data": result,
-    }
-    if not request.GET:
-        return render(request, "app_vadim_zharski/task_04.html", content)
-
-    string = str(request.GET["string"])
-    result = les4.task_04_palindrom(string)
-    payload: dict = {
-        "string": string,
-        "data": result,
-    }
-    return HttpResponse(json.dumps(payload), content_type="application/json")
+    if request.GET:
+        string = str(request.GET["string"])
+        result = les4.task_04_palindrom(string)
+        content: dict = {
+            "data": {
+                "string": string,
+                "result": result,
+            }}
+        if 'html' in request.GET:
+            return render(request, "app_vadim_zharski/task_04.html", content)
+        if 'json' in request.GET:
+            return HttpResponse(json.dumps(content), content_type="application/json")
+    return render(request, "app_vadim_zharski/task_04.html")
 
 
 def task_06_boundary(request: HttpRequest) -> HttpResponse:
-    arr: str = ""
-    result: str | tuple = ""
-    content: dict = {
-        "arr": arr,
-        "data": result,
-    }
-    if not request.GET:
-        return render(request, "app_vadim_zharski/task_06_01.html", content)
-
-    arr = str(request.GET["arr"])
-    result = les6.task_01_boundary(arr)
-    payload: dict = {
-        "arr": arr,
-        "data": result,
-    }
-    return HttpResponse(json.dumps(payload), content_type="application/json")
+    if request.GET:
+        arr = str(request.GET["arr"])
+        result = les6.task_01_boundary(arr)
+        content: dict = {
+            "data": {
+                "arr": arr,
+                "result": result,
+            }}
+        if 'html' in request.GET:
+            return render(request, "app_vadim_zharski/task_06_01.html", content)
+        if 'json' in request.GET:
+            return HttpResponse(json.dumps(content), content_type="application/json")
+    return render(request, "app_vadim_zharski/task_06_01.html")
